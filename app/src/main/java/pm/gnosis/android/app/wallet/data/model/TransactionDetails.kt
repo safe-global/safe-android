9bdec8f75283d63b19c7f68ca6e0502efc15d482
fmrsabino <fredericomsabino@gmail.com>
2017-07-25 22:50:15 +0200

    Add gasPrice and nonce to the ERC67 parser

--- app/src/main/java/pm/gnosis/android/app/wallet/data/model/TransactionDetails.kt
+++ app/src/main/java/pm/gnosis/android/app/wallet/data/model/TransactionDetails.kt
@@ -2,26 +2,30 @@ package pm.gnosis.android.app.wallet.data.model
 
 import android.os.Parcel
 import android.os.Parcelable
-import pm.gnosis.android.app.wallet.util.decimalAsBigInteger
-import pm.gnosis.android.app.wallet.util.hexAsBigInteger
-import pm.gnosis.android.app.wallet.util.nullOnThrow
+import pm.gnosis.android.app.wallet.util.*
 import java.math.BigInteger
 
 data class TransactionDetails(val address: BigInteger,
                               val value: Wei? = null,
                               val gas: BigInteger? = null,
-                              val data: String? = null) : Parcelable {
+                              val gasPrice: BigInteger? = null,
+                              val data: String? = null,
+                              val nonce: BigInteger? = null) : Parcelable {
     constructor(parcel: Parcel) : this(
             parcel.readString().hexAsBigInteger(),
             nullOnThrow { Wei(parcel.readString().hexAsBigInteger()) },
             nullOnThrow { parcel.readString().decimalAsBigInteger() },
-            parcel.readString())
+            nullOnThrow { parcel.readString().decimalAsBigInteger() },
+            parcel.readString(),
+            nullOnThrow { parcel.readString().decimalAsBigInteger() })
 
     override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(address.toString(16))
-        parcel.writeString(value?.value?.toString(10))
-        parcel.writeString(gas?.toString(10))
+        parcel.writeString(address.asHexString())
+        parcel.writeString(value?.value?.asDecimalString())
+        parcel.writeString(gas?.asDecimalString())
+        parcel.writeString(gasPrice?.asDecimalString())
         parcel.writeString(data)
+        parcel.writeString(nonce?.asDecimalString())
     }
 
     override fun describeContents() = 0
