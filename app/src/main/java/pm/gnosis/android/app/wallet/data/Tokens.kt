e167d1fe6c70a278eeb0d3952bb1d75c96420c3e
fmrsabino <fredericomsabino@gmail.com>
2017-07-26 20:25:49 +0200

    Replace token interface with Token object (holder)
    Since token information should now be retrieved using an RPC call we now only store some known token addresses

---
+++ app/src/main/java/pm/gnosis/android/app/wallet/data/Tokens.kt
@@ -0,0 +1,16 @@
+package pm.gnosis.android.app.wallet.data
+
+import pm.gnosis.android.app.wallet.util.hexAsBigInteger
+
+object Token {
+    val Qtum = "0x9a642d6b3368ddc662CA244bAdf32cDA716005BC".hexAsBigInteger()
+    val Iconomi = "0x888666CA69E0f178DED6D75b5726Cee99A87D698".hexAsBigInteger()
+    val Gnosis = "0x6810e776880c02933d47db1b9fc05908e5386b96".hexAsBigInteger()
+    val Golem = "0xa74476443119A942dE498590Fe1f2454d7D4aC0d".hexAsBigInteger()
+    val StatusNetwork = "0x744d70fdbe2ba4cf95131626614a1763df805b9e".hexAsBigInteger()
+    val Augur = "0x48c80F1f4D53D5951e5D5438B54Cba84f29F32a5".hexAsBigInteger()
+    val Maker = "0xc66ea802717bfb9833400264dd12c2bceaa34a6d".hexAsBigInteger()
+    val DigixGlobal = "0xe0b7927c4af23765cb51314a0e0521a9645f0e2a".hexAsBigInteger()
+    val OmiseGO = "0xd26114cd6EE289AccF82350c8d8487fedB8A0C07".hexAsBigInteger()
+    val BasicAttentionToken = "0x0d8775f648430679a709e98d2b0cb6250d2887ef".hexAsBigInteger()
+}
