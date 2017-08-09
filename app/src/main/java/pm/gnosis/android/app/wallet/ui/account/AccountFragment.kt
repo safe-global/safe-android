46e4a5da3191cb0a9a68aeae49fcf9f7aff3d06a
fmrsabino <fredericomsabino@gmail.com>
2017-08-09 12:53:28 +0200

    Add QRCode generation

--- app/src/main/java/pm/gnosis/android/app/wallet/ui/account/AccountFragment.kt
+++ app/src/main/java/pm/gnosis/android/app/wallet/ui/account/AccountFragment.kt
@@ -1,12 +1,15 @@
 package pm.gnosis.android.app.wallet.ui.account
 
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.rxkotlin.plusAssign
 import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.fragment_account.*
 import pm.gnosis.android.app.wallet.R
 import pm.gnosis.android.app.wallet.data.model.Wei
@@ -15,6 +18,7 @@ import pm.gnosis.android.app.wallet.di.component.DaggerViewComponent
 import pm.gnosis.android.app.wallet.di.module.ViewModule
 import pm.gnosis.android.app.wallet.ui.base.BaseFragment
 import pm.gnosis.android.app.wallet.util.copyToClipboard
+import pm.gnosis.android.app.wallet.util.generateQrCode
 import pm.gnosis.android.app.wallet.util.shareExternalText
 import pm.gnosis.android.app.wallet.util.snackbar
 import timber.log.Timber
@@ -23,6 +27,9 @@ import javax.inject.Inject
 
 class AccountFragment : BaseFragment() {
     @Inject lateinit var presenter: AccountPresenter
+
+    private var qrCodeIsLoading = false
+    private var balanceIsLoading = false
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater?.inflate(R.layout.fragment_account, container, false)
@@ -41,6 +48,10 @@ class AccountFragment : BaseFragment() {
         fragment_account_share.setOnClickListener {
             context.shareExternalText(fragment_account_address.text.toString(), "Share your address")
         }
+
+        fragment_account_qrcode.setOnClickListener {
+            disposables += generateQrCodeDisposable(fragment_account_address.text.toString())
+        }
     }
 
     private fun accountBalanceDisposable() = presenter.getAccountBalance()
@@ -49,17 +60,44 @@ class AccountFragment : BaseFragment() {
             .doOnTerminate { onAccountBalanceLoading(isLoading = false) }
             .subscribeBy(onNext = this::onAccountBalance, onError = this::onAccountBalanceError)
 
+    private fun generateQrCodeDisposable(contents: String) =
+            Single.fromCallable { generateQrCode(contents) }
+                    .subscribeOn(Schedulers.computation())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .doOnSubscribe { onQrCodeLoading(true) }
+                    .doAfterTerminate { onQrCodeLoading(false) }
+                    .subscribeBy(onSuccess = this::onQrCode, onError = this::onQrCodeError)
+
     private fun onAccountBalance(wei: Wei) {
         val etherBalance = wei.toEther()
         fragment_account_balance.text = if (etherBalance.compareTo(BigDecimal.ZERO) == 0) "0 Ξ" else etherBalance.stripTrailingZeros().toPlainString() + " Ξ"
     }
 
     private fun onAccountBalanceLoading(isLoading: Boolean) {
-        progress_bar.visibility = if (isLoading) View.VISIBLE else View.GONE
+        balanceIsLoading = isLoading
+        updateProgressView()
     }
 
     private fun onAccountBalanceError(throwable: Throwable) {
         Timber.e(throwable)
+    }
+
+    private fun onQrCode(bitmap: Bitmap) {
+        Timber.d(bitmap.toString())
+    }
+
+    private fun onQrCodeLoading(isLoading: Boolean) {
+        qrCodeIsLoading = isLoading
+        updateProgressView()
+    }
+
+    private fun onQrCodeError(throwable: Throwable) {
+        Timber.e(throwable)
+    }
+
+    private fun updateProgressView() {
+        fragment_account_progress_bar.visibility =
+                if (qrCodeIsLoading || balanceIsLoading) View.VISIBLE else View.INVISIBLE
     }
 
     override fun inject(component: ApplicationComponent) {
