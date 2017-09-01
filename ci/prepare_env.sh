f54679f10586382bdb15347b32e2ee42569ec22b
Richard Meissner <RiMe@gmx.net>
2017-09-01 17:15:20 +0200

    Prepare for CI

---
+++ ci/prepare_env.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# fail if any commands fails
+set -e
+
+# write your script here
+export APP_VERSION_CODE=$TRAVIS_BUILD_NUMBER
+# strip the first char as that should always be "v" (as tags should be in the format "vX.X.X")
+description="$(git describe --tags)"
+export APP_VERSION_NAME=${description:1}
+export APP_RELEASE_NOTES=$TRAVIS_COMMIT_MESSAGE
