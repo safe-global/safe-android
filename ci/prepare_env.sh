327df60a9aed952c3a110135518ef9c7b70ccb27
Richard Meissner <msc.meissner@gmail.com>
2017-09-01 20:49:43 +0200

    Changed script permissions

--- ci/prepare_env.sh
+++ ci/prepare_env.sh
@@ -2,6 +2,8 @@
 # fail if any commands fails
 set -e
 
+git branch
+
 # write your script here
 export APP_VERSION_CODE=$TRAVIS_BUILD_NUMBER
 # strip the first char as that should always be "v" (as tags should be in the format "vX.X.X")
