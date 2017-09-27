058705385080fc91e1a1c5eb3c81e5b80d877e3a
Richard Meissner <msc.meissner@gmail.com>
2017-09-27 13:06:24 +0200

    FU gradle and co

--- accounts/kethereum/src/main/java/pm/gnosis/heimdall/accounts/data/db/AccountsDatabase.kt
+++ accounts/kethereum/src/main/java/pm/gnosis/heimdall/accounts/data/db/AccountsDatabase.kt
@@ -1,6 +1,7 @@
 package pm.gnosis.heimdall.accounts.data.db
 
 import android.arch.persistence.room.Database
+import android.arch.persistence.room.RoomDatabase
 import pm.gnosis.heimdall.accounts.repositories.impl.models.db.AccountDb
 
 @Database(entities = arrayOf(AccountDb::class), version = 1)
--- accounts/kethereum/src/main/java/pm/gnosis/heimdall/accounts/di/AccountsModule.kt
+++ accounts/kethereum/src/main/java/pm/gnosis/heimdall/accounts/di/AccountsModule.kt
@@ -1,5 +1,6 @@
 package pm.gnosis.heimdall.accounts.di
 
+import android.arch.persistence.room.Room
 import android.content.Context
 import dagger.Module
 import dagger.Provides
