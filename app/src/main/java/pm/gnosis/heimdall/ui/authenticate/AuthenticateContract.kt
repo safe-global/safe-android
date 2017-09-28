dcae25ca42b0f4977ecea4e7b30d9f444e996dad
Richard Meissner <msc.meissner@gmail.com>
2017-09-28 23:47:38 +0200

    Closes #30: Adjust Authenticate Screen

---
+++ app/src/main/java/pm/gnosis/heimdall/ui/authenticate/AuthenticateContract.kt
@@ -0,0 +1,13 @@
+package pm.gnosis.heimdall.ui.authenticate
+
+import android.arch.lifecycle.ViewModel
+import android.content.Intent
+import io.reactivex.Observable
+import pm.gnosis.heimdall.common.util.Result
+
+
+abstract class AuthenticateContract: ViewModel() {
+    abstract fun checkResult(result: ActivityResults): Observable<Result<Intent>>
+
+    data class ActivityResults(val requestCode: Int, val resultCode: Int, val data: Intent?)
+}
