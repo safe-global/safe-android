c09a10d0a5341dc43e9dbeb606b00384c0a31a18
Frederico Sabino <fredericomsabino@gmail.com>
2017-11-15 11:11:44 +0100

    Update dependencies (#74)

--- app/src/test/java/pm/gnosis/heimdall/test/utils/MockUtils.kt
+++ app/src/test/java/pm/gnosis/heimdall/test/utils/MockUtils.kt
@@ -1,24 +1,9 @@
 package pm.gnosis.heimdall.test.utils
 
-import org.mockito.Answers
 import org.mockito.Mockito
-import org.mockito.internal.creation.MockSettingsImpl
-import org.mockito.internal.util.MockUtil
 
 
 object MockUtils {
-    fun <T> anyObject(clzz: Class<T>): T {
-        Mockito.any<T>()
-        return uncheckedMock(clzz)
-    }
-
-    @Suppress("UNCHECKED_CAST")
-    private fun <T> uncheckedMock(clzz: Class<T>): T {
-        val impl = MockSettingsImpl<T>().defaultAnswer(Answers.RETURNS_DEFAULTS) as MockSettingsImpl<T>
-        val creationSettings = impl.confirm(clzz)
-        return MockUtil.createMock(creationSettings)
-    }
-
     fun <T> eq(value: T): T {
         Mockito.eq(value)
         return uninitialized()
