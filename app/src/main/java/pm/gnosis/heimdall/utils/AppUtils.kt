41e07815e3146b6fc6efb65fc34666bf6415a2c6
Richard Meissner <rmeissner@users.noreply.github.com>
2017-12-13 12:23:03 +0100

    Fixed authenticate screen (#90)

--- app/src/main/java/pm/gnosis/heimdall/utils/AppUtils.kt
+++ app/src/main/java/pm/gnosis/heimdall/utils/AppUtils.kt
@@ -2,15 +2,16 @@ package pm.gnosis.heimdall.utils
 
 import android.app.Activity
 import android.content.Intent
+import android.support.design.widget.Snackbar
 import android.view.View
 import pm.gnosis.heimdall.R
 import pm.gnosis.heimdall.common.utils.ZxingIntentIntegrator
 import pm.gnosis.heimdall.common.utils.snackbar
 import pm.gnosis.heimdall.ui.exceptions.LocalizedException
 
-fun errorSnackbar(view: View, throwable: Throwable) {
+fun errorSnackbar(view: View, throwable: Throwable, duration: Int = Snackbar.LENGTH_LONG) {
     val message = (throwable as? LocalizedException)?.localizedMessage() ?: view.context.getString(R.string.error_try_again)
-    snackbar(view, message)
+    snackbar(view, message, duration)
 }
 
 fun handleQrCodeActivityResult(requestCode: Int, resultCode: Int, data: Intent?,
