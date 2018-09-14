9208c9eee27d7a756dc48eaff123cc15bb727719
Richard Meissner <msc.meissner@gmail.com>
2018-09-14 11:22:28 +0200

    More fun

--- ci/build_debug.sh
+++ ci/build_debug.sh
@@ -2,4 +2,10 @@
 # fail if any commands fails
 set -e
 
-./gradlew clean assembleDebug createDebugTestCoverage connectedCheck --stacktrace
+./gradlew clean assembleDebug createDebugTestCoverage -PdisablePreDex --stacktrace
+android-wait-for-emulator
+adb shell settings put global window_animation_scale 0 &
+adb shell settings put global transition_animation_scale 0 &
+adb shell settings put global animator_duration_scale 0 &
+adb shell input keyevent 82 &
+./gradlew connectedCheck -PdisablePreDex --stacktrace
