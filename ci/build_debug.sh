703ae3dca86d760f3459200c5095b9c953de33ab
Richard Meissner <msc.meissner@gmail.com>
2018-09-14 15:07:55 +0200

    Another one bites the dust

--- ci/build_debug.sh
+++ ci/build_debug.sh
@@ -8,4 +8,5 @@ adb shell settings put global window_animation_scale 0 &
 adb shell settings put global transition_animation_scale 0 &
 adb shell settings put global animator_duration_scale 0 &
 adb shell input keyevent 82 &
+adb devices
 ./gradlew connectedCheck -PdisablePreDex --stacktrace
