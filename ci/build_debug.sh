a4ce0733fe6ef4a61a9357d59759072970031a55
Richard Meissner <msc.meissner@gmail.com>
2018-09-25 10:10:42 +0200

    Remove UI tests from travis for now

--- ci/build_debug.sh
+++ ci/build_debug.sh
@@ -2,11 +2,4 @@
 # fail if any commands fails
 set -e
 
-./gradlew clean assembleDebug createDebugTestCoverage -PdisablePreDex --stacktrace
-android-wait-for-emulator
-adb shell settings put global window_animation_scale 0 &
-adb shell settings put global transition_animation_scale 0 &
-adb shell settings put global animator_duration_scale 0 &
-adb shell input keyevent 82 &
-adb devices
-./gradlew connectedCheck -PdisablePreDex --stacktrace
+./gradlew clean assembleDebug createDebugTestCoverage --stacktrace
