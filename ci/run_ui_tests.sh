87d2b2b50569cd8879c3d64b185b3d2ce50c9998
Richard Meissner <msc.meissner@gmail.com>
2018-10-20 22:12:24 +0200

    Adjust buildkite steps

--- ci/run_ui_tests.sh
+++ ci/run_ui_tests.sh
@@ -6,5 +6,8 @@ set -e
 
 ./ci/wait_for_emulator.sh
 
+# Buildkite uses a clean state for each step (for concurrency)
+./ci/prepare_env_buildkite.sh
+
 echo "Execute UI tests"
 ./gradlew clean assembleDebug connectedDebugAndroidTest --stacktrace
