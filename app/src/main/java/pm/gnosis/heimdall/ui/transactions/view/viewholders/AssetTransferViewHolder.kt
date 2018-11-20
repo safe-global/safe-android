83218762d2bbc6b6a793a3a4761f059d21ba2722
Frederico Sabino <fredericomsabino@gmail.com>
2018-11-20 10:54:26 +0100

    Use all the decimals when reviewing an asset transfer. Closes #297 (#302)

--- app/src/main/java/pm/gnosis/heimdall/ui/transactions/view/viewholders/AssetTransferViewHolder.kt
+++ app/src/main/java/pm/gnosis/heimdall/ui/transactions/view/viewholders/AssetTransferViewHolder.kt
@@ -61,7 +61,7 @@ class AssetTransferViewHolder(
             .subscribeBy(
                 onSuccess = {
                     view?.apply {
-                        layout_asset_transfer_info_value.text = it.displayString(data.amount)
+                        layout_asset_transfer_info_value.text = it.displayString(data.amount, it.decimals)
                         loadSafeTokenBalance(it)
                     }
                 },
