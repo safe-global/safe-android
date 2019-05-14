87095fb9dc693db7ac86e5c1574814ab03235477
Vitaly Katz <vitaly@gnosis.pm>
2019-05-14 14:29:38 +0200

    Color status bar

--- app/src/main/java/pm/gnosis/heimdall/ui/base/ViewModelActivity.kt
+++ app/src/main/java/pm/gnosis/heimdall/ui/base/ViewModelActivity.kt
@@ -1,9 +1,13 @@
 package pm.gnosis.heimdall.ui.base
 
+import android.os.Build
 import androidx.lifecycle.ViewModel
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.annotation.LayoutRes
+import pm.gnosis.heimdall.R
 import pm.gnosis.heimdall.di.components.ViewComponent
+import pm.gnosis.svalinn.common.utils.getColorCompat
 import javax.inject.Inject
 
 abstract class ViewModelActivity<VM : ViewModel> : BaseActivity() {
@@ -19,5 +23,13 @@ abstract class ViewModelActivity<VM : ViewModel> : BaseActivity() {
         super.onCreate(savedInstanceState)
         inject(viewComponent())
         setContentView(layout())
+    }
+
+    protected fun colorStatusBar() {
+        if (Build.VERSION.SDK_INT >= 21) {
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            window.statusBarColor = getColorCompat(R.color.aqua_blue)
+        }
     }
 }
