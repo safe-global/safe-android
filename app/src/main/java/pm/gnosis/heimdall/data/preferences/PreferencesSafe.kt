33b2efdfafbff84ac263a3d42bfd01be1a288ec7
Vitaly Katz <vitaly@gnosis.pm>
2019-07-10 14:58:34 +0200

    Refactor fabric preferences

--- app/src/main/java/pm/gnosis/heimdall/data/preferences/PreferencesSafe.kt
+++ app/src/main/java/pm/gnosis/heimdall/data/preferences/PreferencesSafe.kt
@@ -6,6 +6,7 @@ import pm.gnosis.model.Solidity
 import pm.gnosis.svalinn.common.utils.edit
 import pm.gnosis.utils.asEthereumAddress
 import pm.gnosis.utils.asEthereumAddressString
+import java.util.*
 
 class PreferencesSafe(context: Context) {
 
@@ -27,9 +28,23 @@ class PreferencesSafe(context: Context) {
             prefs.edit { putString(KEY_LAST_SYNC_PUSH_INFO, value) }
         }
 
+    var installId: String
+        get() = prefs.getString(KEY_INSTALL_ID, null) ?: generateAndStoreInstallId()
+        set(value) {
+            prefs.edit { putString(KEY_INSTALL_ID, value) }
+        }
+
+
+    private fun generateAndStoreInstallId(): String {
+        val id = UUID.randomUUID().toString()
+        installId = id
+        return id
+    }
+
     companion object {
         const val PREFERENCES_SAFE = "GnosisPrefs"
         private const val KEY_SELECTED_SAFE = "safe_main.string.selected_safe"
         private const val KEY_LAST_SYNC_PUSH_INFO = "prefs.string.accounttoken"
+        private const val KEY_INSTALL_ID = "fabric_core.string.install_id"
     }
 }
--- app/src/main/java/pm/gnosis/heimdall/reporting/impl/FabricCore.kt
+++ app/src/main/java/pm/gnosis/heimdall/reporting/impl/FabricCore.kt
@@ -1,41 +1,23 @@
 package pm.gnosis.heimdall.reporting.impl
 
 import android.content.Context
-import android.content.SharedPreferences
 import com.crashlytics.android.Crashlytics
 import io.fabric.sdk.android.Fabric
+import pm.gnosis.heimdall.data.preferences.PreferencesSafe
 import pm.gnosis.heimdall.di.ApplicationContext
-import pm.gnosis.svalinn.common.PreferencesManager
-import pm.gnosis.svalinn.common.utils.edit
-import java.util.*
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class FabricCore @Inject constructor(
     @ApplicationContext context: Context,
-    preferencesManager: PreferencesManager
+    prefs: PreferencesSafe
 ) {
     val instance = Fabric.with(context, Crashlytics())!!
 
-    val installId: String
+    val installId: String = prefs.installId
 
     init {
-        installId = preferencesManager.prefs.let {
-            it.getString(INSTALL_ID_KEY, null) ?: it.generateAndStoreInstallId()
-        }
         Crashlytics.setUserIdentifier(installId)
-    }
-
-    private fun SharedPreferences.generateAndStoreInstallId(): String {
-        val installId = UUID.randomUUID().toString()
-        edit {
-            putString(INSTALL_ID_KEY, installId)
-        }
-        return installId
-    }
-
-    companion object {
-        private const val INSTALL_ID_KEY = "fabric_core.string.install_id"
     }
 }
