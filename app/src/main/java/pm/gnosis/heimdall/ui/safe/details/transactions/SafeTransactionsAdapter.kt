a6048b933263bd1e68f8a95c2363936fa2d2fff1
Richard Meissner <richard@gnosis.io>
2019-09-09 11:24:28 +0200

    Use transaction settings icon

--- app/src/main/java/pm/gnosis/heimdall/ui/safe/details/transactions/SafeTransactionsAdapter.kt
+++ app/src/main/java/pm/gnosis/heimdall/ui/safe/details/transactions/SafeTransactionsAdapter.kt
@@ -174,7 +174,7 @@ class SafeTransactionsAdapter @Inject constructor(
                         infoText = context.getString(R.string.replaced_recovery_phrase),
                         valueText = null,
                         valueColor = R.color.blue,
-                        iconRes = R.drawable.ic_transaction_white_24dp // TODO: use settings icon
+                        iconRes = R.drawable.ic_transaction_settings
                     )
                 }
                 is TransactionData.ConnectExtension -> {
@@ -183,7 +183,7 @@ class SafeTransactionsAdapter @Inject constructor(
                         infoText = context.getString(R.string.connected_extension),
                         valueText = null,
                         valueColor = R.color.blue,
-                        iconRes = R.drawable.ic_transaction_white_24dp // TODO: use settings icon
+                        iconRes = R.drawable.ic_transaction_settings
                     )
                 }
                 is TransactionData.UpdateMasterCopy -> {
@@ -192,7 +192,7 @@ class SafeTransactionsAdapter @Inject constructor(
                         infoText = context.getString(R.string.contract_upgrade),
                         valueText = null,
                         valueColor = R.color.blue,
-                        iconRes = R.drawable.ic_transaction_white_24dp // TODO: use settings icon
+                        iconRes = R.drawable.ic_transaction_settings
                     )
                 }
             }
