c0c7e8e4ac913229c29fe62ef14ce20d3e08d102
Vitaly Katz <vitaly@gnosis.pm>
2019-12-02 14:09:05 +0100

    Adjust recovering safe submit layout

--- app/src/main/java/pm/gnosis/heimdall/ui/safe/recover/safe/submit/RecoveringSafeSubmitFragment.kt
+++ app/src/main/java/pm/gnosis/heimdall/ui/safe/recover/safe/submit/RecoveringSafeSubmitFragment.kt
@@ -4,9 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.jakewharton.rxbinding2.view.clicks
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.rxkotlin.plusAssign
+import io.reactivex.rxkotlin.subscribeBy
 import kotlinx.android.synthetic.main.layout_recovering_safe_submit.*
 import pm.gnosis.heimdall.R
 import pm.gnosis.heimdall.data.repositories.models.RecoveringSafe
@@ -19,7 +19,9 @@ import pm.gnosis.heimdall.reporting.EventTracker
 import pm.gnosis.heimdall.reporting.ScreenId
 import pm.gnosis.heimdall.ui.base.BaseFragment
 import pm.gnosis.heimdall.ui.safe.main.SafeMainActivity
+import pm.gnosis.heimdall.utils.InfoTipDialogBuilder
 import pm.gnosis.heimdall.utils.errorSnackbar
+import pm.gnosis.model.Solidity
 import pm.gnosis.svalinn.common.utils.mapToResult
 import pm.gnosis.svalinn.common.utils.subscribeForResult
 import pm.gnosis.svalinn.common.utils.visible
@@ -27,6 +29,12 @@ import pm.gnosis.svalinn.common.utils.withArgs
 import pm.gnosis.utils.asEthereumAddress
 import pm.gnosis.utils.asEthereumAddressString
 import javax.inject.Inject
+import kotlinx.android.synthetic.main.include_transfer_summary_final.transfer_data_fees_error as feesError
+import kotlinx.android.synthetic.main.include_transfer_summary_final.transfer_data_fees_info as feesInfo
+import kotlinx.android.synthetic.main.include_transfer_summary_final.transfer_data_fees_value as feesValue
+import kotlinx.android.synthetic.main.include_transfer_summary_final.transfer_data_safe_balance_after_value as balanceAfterValue
+import kotlinx.android.synthetic.main.include_transfer_summary_final.transfer_data_safe_balance_before_value as balanceBeforeValue
+import kotlinx.android.synthetic.main.layout_recovering_safe_submit.recover_safe_swipe_to_refresh as swipeToRefresh
 
 class RecoveringSafeSubmitFragment : BaseFragment() {
 
@@ -45,63 +53,84 @@ class RecoveringSafeSubmitFragment : BaseFragment() {
     @Inject
     lateinit var eventTracker: EventTracker
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
-        inflater.inflate(R.layout.layout_recovering_safe_submit, container, false)
+    protected lateinit var safe: Solidity.Address
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
+            = inflater.inflate(R.layout.layout_recovering_safe_submit, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        recover_safe_bottom_panel.disabled = true
+
+        safe = arguments?.getString(EXTRA_SAFE_ADDRESS)?.asEthereumAddress() ?: throw IllegalStateException("No safe address provided!")
+
+        addressHelper.populateAddressInfo(
+            recover_safe_submit_info_safe_address,
+            recover_safe_submit_info_safe_name,
+            recover_safe_submit_info_safe_image,
+            safe
+        ).forEach { disposables += it }
+
+        swipeToRefresh.setOnRefreshListener {
+            loadExecuteInfo()
+        }
+
+        feesInfo.setOnClickListener {
+            InfoTipDialogBuilder.build(context!!, R.layout.dialog_network_fee, R.string.ok).show()
+        }
+
+    }
 
     override fun onStart() {
         super.onStart()
         eventTracker.submit(Event.ScreenView(ScreenId.RECOVER_SAFE_REVIEW))
 
-        layout_recovering_safe_submit_button.isEnabled = false
-        layout_recovering_safe_submit_button.visible(true)
-        layout_recovering_safe_submit_retry.isEnabled = false
-        layout_recovering_safe_submit_retry.visible(false)
-        val safeAddress = arguments?.getString(EXTRA_SAFE_ADDRESS)?.asEthereumAddress() ?: throw IllegalStateException("No safe address provided!")
-        addressHelper.populateAddressInfo(
-            layout_recovering_safe_submit_info_safe_address,
-            layout_recovering_safe_submit_info_safe_name,
-            layout_recovering_safe_submit_info_safe_image,
-            safeAddress
-        ).forEach { disposables += it }
-
-        // Automatically load on start
-        disposables += layout_recovering_safe_submit_retry.clicks().startWith(Unit)
+        disposables += recover_safe_bottom_panel.forwardClicks
             .doOnNext {
-                layout_recovering_safe_submit_retry.isEnabled = false
-                layout_recovering_safe_submit_button.isEnabled = false
-
+                onLoading(true)
             }
-            .switchMapSingle { viewModel.loadRecoveryExecuteInfo(safeAddress).mapToResult() }
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribeForResult(onNext = {
-                layout_recovering_safe_submit_data_balance_value.text = it.paymentToken.displayString(it.balance)
-                layout_recovering_safe_submit_data_fees_value.text = it.paymentToken.displayString(it.paymentAmount)
-                layout_recovering_safe_submit_data_fees_error.visible(!it.canSubmit)
-                layout_recovering_safe_submit_button.visible(it.canSubmit)
-                layout_recovering_safe_submit_button.isEnabled = it.canSubmit
-                layout_recovering_safe_submit_retry.visible(!it.canSubmit)
-                layout_recovering_safe_submit_retry.isEnabled = !it.canSubmit
-            }, onError = {
-                errorSnackbar(layout_recovering_safe_submit_button, it)
-                layout_recovering_safe_submit_button.visible(false)
-                layout_recovering_safe_submit_retry.isEnabled = true
-                layout_recovering_safe_submit_retry.visible(true)
-            })
-
-        disposables += layout_recovering_safe_submit_button.clicks()
-            .doOnNext {
-                layout_recovering_safe_submit_retry.isEnabled = false
-                layout_recovering_safe_submit_button.isEnabled = false
-            }
-            .switchMapSingle { viewModel.submitRecovery(safeAddress).mapToResult() }
+            .switchMapSingle { viewModel.submitRecovery(safe).mapToResult() }
             .observeOn(AndroidSchedulers.mainThread())
             .subscribeForResult(onNext = {
                 startActivity(SafeMainActivity.createIntent(context!!, it))
             }, onError = {
-                errorSnackbar(layout_recovering_safe_submit_button, it)
-                layout_recovering_safe_submit_button.isEnabled = true
-                layout_recovering_safe_submit_retry.isEnabled = true
+                errorSnackbar(recover_safe_bottom_panel, it)
+                onLoading(false)
             })
+    }
+
+    override fun onResume() {
+        super.onResume()
+        loadExecuteInfo()
+    }
+
+    private fun loadExecuteInfo() {
+        onLoading(true)
+        viewModel.loadRecoveryExecuteInfo(safe)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeBy(
+                {
+                    onLoading(false)
+                    errorSnackbar(recover_safe_bottom_panel, it)
+                },
+                {
+                    balanceBeforeValue.text = it.paymentToken.displayString(it.balance)
+                    balanceAfterValue.text = it.paymentToken.displayString(it.balance - it.paymentAmount)
+                    feesValue.text = it.paymentToken.displayString(it.paymentAmount)
+                    feesError.visible(!it.canSubmit)
+                    recover_safe_bottom_panel.disabled = !it.canSubmit
+                    onLoading(false)
+                }
+            )
+    }
+
+    private fun onLoading(isLoading: Boolean) {
+        recover_safe_bottom_panel.disabled = isLoading
+        recover_safe_progress_bar.visible(isLoading)
+        swipeToRefresh.isRefreshing = false
+        swipeToRefresh.isActivated = !isLoading
+        swipeToRefresh.isEnabled = !isLoading
     }
 
     companion object {
