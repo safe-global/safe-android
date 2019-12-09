1b7d963e22c191a82904c1b8366dd1b9a48016e9
Vitaly Katz <vitaly@gnosis.pm>
2019-12-09 11:06:46 +0100

    Prepare buildkite env before running unit tests

--- ci/run_unit_tests.sh
+++ ci/run_unit_tests.sh
@@ -2,4 +2,7 @@
 # fail if any commands fails
 set -e
 
+# Buildkite uses a clean state for each step (for concurrency)
+./ci/prepare_env_buildkite.sh
+
 ./gradlew clean assembleDebug testDebugUnitTest --stacktrace
