2d9f47c6a8dd1ccd1285894e063855147a099d01
Vitaly Katz <vitaly@gnosis.pm>
2020-01-07 15:01:28 +0100

    Remove stepper from 2fa intro screens (#520)

--- app/src/main/java/pm/gnosis/heimdall/ui/two_factor/keycard/KeycardIntroActivity.kt
+++ app/src/main/java/pm/gnosis/heimdall/ui/two_factor/keycard/KeycardIntroActivity.kt
@@ -5,15 +5,12 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import kotlinx.android.synthetic.main.layout_keycard_intro.*
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import pm.gnosis.heimdall.R
 import pm.gnosis.heimdall.helpers.NfcActivity
 import pm.gnosis.heimdall.reporting.ScreenId
-import pm.gnosis.heimdall.ui.base.BaseActivity
 import pm.gnosis.heimdall.utils.AuthenticatorSetupInfo
 import pm.gnosis.heimdall.utils.put
 import pm.gnosis.model.Solidity
-import pm.gnosis.svalinn.common.utils.visible
 import pm.gnosis.utils.asEthereumAddress
 import pm.gnosis.utils.asEthereumAddressString
 
@@ -24,8 +21,6 @@ class KeycardIntroActivity : NfcActivity(), KeycardPairingDialog.PairingCallback
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_keycard_intro)
-
-        step_indicator.visible(intent.getBooleanExtra(EXTRA_ONBOARDING, false))
 
         keycard_intro_back_button.setOnClickListener { onBackPressed() }
         keycard_intro_setup.setOnClickListener {
