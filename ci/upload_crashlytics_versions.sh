059d73551679215339a3130b7a263bc298cdee96
Vitaly Katz <vitaly@gnosis.pm>
2020-01-16 11:44:09 +0100

    test

--- ci/upload_crashlytics_versions.sh
+++ ci/upload_crashlytics_versions.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Buildkite uses a clean state for each step (for concurrency)
-./ci/prepare_env_buildkite.sh
+source ./ci/prepare_env_buildkite.sh
 
 # Buildkite branch equals to tag name if build was triggered by tag
 if [[ $BUILKITE_BRANCH  =~ /^v[0-9]+.*/ ]]; then
