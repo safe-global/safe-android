a98d5f9bf1c5c7edee20584fde3ff89d31704941
Vitaly Katz <vitaly@gnosis.pm>
2020-01-16 11:00:12 +0100

    Adjust crashlytics upload script

--- ci/upload_crashlytics_versions.sh
+++ ci/upload_crashlytics_versions.sh
@@ -6,8 +6,7 @@ set -e
 ./ci/prepare_env_buildkite.sh
 
 # Buildkite branch equals to tag name if build was triggered by tag
-if [[ $BUILKITE_BRANCH != 'master' ]]
-then
+if [[ $BUILKITE_BRANCH  =~ /^v[0-9]+.*/ ]]; then
     export APP_VERSION_NAME=${BUILDKITE_BRANCH:1}
 fi
 
