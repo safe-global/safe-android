9a9de4c6e7d8e7e13b771f4432d08533467202dd
Vitaly Katz <vitaly@gnosis.pm>
2020-01-21 09:16:33 +0100

    Use .io endpoints (#533)

--- ci/upload_crashlytics_versions.sh
+++ ci/upload_crashlytics_versions.sh
@@ -6,7 +6,7 @@ set -e
 source ./ci/prepare_env_buildkite.sh
 
 # Buildkite branch equals to tag name if build was triggered by tag
-if [[ $BUILKITE_BRANCH  =~ /^v[0-9]+.*/ ]]; then
+if [[ $BUILKITE_BRANCH  =~ ^v[0-9]+.* ]]; then
     export APP_VERSION_NAME=${BUILDKITE_BRANCH:1}
 fi
 
