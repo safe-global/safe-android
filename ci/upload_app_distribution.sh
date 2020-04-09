e0a684bc50482d5fec242171fbbb381a94246ed5
Vitaly Katz <vitaly@gnosis.pm>
2020-04-09 15:59:37 +0200

    Replace crashlytics upload script with app distribution; Disable upload (#548)

--- ci/upload_app_distribution.sh
+++ ci/upload_app_distribution.sh
@@ -5,6 +5,8 @@ set -e
 # Buildkite uses a clean state for each step (for concurrency)
 source ./ci/prepare_env_buildkite.sh
 
+export FIREBASE_GROUP="internal-beta"
+
 # Buildkite branch equals to tag name if build was triggered by tag
 if [[ $BUILKITE_BRANCH  =~ ^v[0-9]+.* ]]; then
     export APP_VERSION_NAME=${BUILDKITE_BRANCH:1}
@@ -12,5 +14,6 @@ fi
 
 echo "INFURA_API_KEY=$INFURA_API_KEY" > project_keys
 
-./gradlew assembleInternal assembleRinkeby assembleRelease
-./gradlew crashlyticsUploadDistributionInternal crashlyticsUploadDistributionRinkeby crashlyticsUploadDistributionRelease
+#TODO: requires app distribution setup in firebase
+./gradlew assembleInternal
+./gradlew appDistributionUploadInternal
