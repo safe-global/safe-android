1349bbddb63ce0c5182ef0d217827df3e5decfb8
Jose Pablo Alvarez Lacasia <jose.alvarez@gnosis.io>
2020-04-27 14:52:00 +0200

    Added tests to EnsInputViewModel

---
+++ app/src/test/java/io/gnosis/safe/ui/dialogs/EnsInputViewModelTest.kt
@@ -0,0 +1,63 @@
+package io.gnosis.safe.ui.dialogs
+
+import io.gnosis.data.repositories.EnsRepository
+import io.gnosis.safe.di.Repositories
+import io.mockk.*
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import pm.gnosis.model.Solidity
+import java.lang.IllegalStateException
+import java.math.BigInteger
+
+class EnsInputViewModelTest {
+
+    private val ensRepository = mockk<EnsRepository>()
+    private val repositories = mockk<Repositories>().apply {
+        every { ensRepository() } returns ensRepository
+    }
+
+    private val viewModel = EnsInputViewModel(repositories)
+
+    @Test
+    fun `processEnsInput (valid input safeRepository failure) should throw`() = runBlocking {
+        val throwable = IllegalStateException()
+        coEvery { ensRepository.resolve(any()) } throws throwable
+
+        val actual = runCatching { viewModel.processEnsInput("") }
+
+        with(actual) {
+            assert(isFailure)
+            assertEquals(exceptionOrNull(), throwable)
+        }
+        coVerify(exactly = 1) { ensRepository.resolve("") }
+    }
+
+    @Test
+    fun `processEnsInput (invalid input) should throw`() = runBlocking {
+        coEvery { ensRepository.resolve(any()) } returns null
+
+        val actual = runCatching { viewModel.processEnsInput("") }
+
+        with(actual) {
+            assert(isFailure)
+            assert(exceptionOrNull() is AddressNotFound)
+        }
+        coVerify(exactly = 1) { ensRepository.resolve("") }
+    }
+
+    @Test
+    fun `processEnsInput (valid input) should return Address`() = runBlocking {
+        val address = Solidity.Address(BigInteger.ONE)
+        coEvery { ensRepository.resolve(any()) } returns address
+
+        val actual = runCatching { viewModel.processEnsInput("safe.eth") }
+
+        with(actual) {
+            assert(isSuccess)
+            assert(getOrNull() == address)
+        }
+        coVerify(exactly = 1) { ensRepository.resolve("safe.eth") }
+    }
+
+}
