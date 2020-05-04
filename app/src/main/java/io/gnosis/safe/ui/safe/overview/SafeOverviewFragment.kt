a4dfe5b556e92bb18e1123f1aeadf61a9be7c7ac
Dirk Jäckel <dirk.jaeckel@gnosis.pm>
2020-05-04 15:01:33 +0200

    Move logic to ViewModel

--- app/src/main/java/io/gnosis/safe/ui/safe/overview/SafeOverviewFragment.kt
+++ app/src/main/java/io/gnosis/safe/ui/safe/overview/SafeOverviewFragment.kt
@@ -1,25 +1,16 @@
 package io.gnosis.safe.ui.safe.overview
 
-import android.content.Context
 import android.os.Bundle
-import android.text.Html
-import android.text.method.LinkMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.google.android.material.bottomsheet.BottomSheetDialog
-import io.gnosis.safe.R
 import io.gnosis.safe.databinding.FragmentSafeOverviewBinding
 import io.gnosis.safe.di.components.ViewComponent
 import io.gnosis.safe.ui.base.BaseFragment
-import kotlinx.android.synthetic.main.bottom_sheet_terms_and_conditions.*
 import javax.inject.Inject
 
 class SafeOverviewFragment : BaseFragment<FragmentSafeOverviewBinding>() {
-
-    private lateinit var termsBottomSheetDialog: TermsBottomSheetDialog
-
     @Inject
     lateinit var viewModel: SafeOverviewViewModel
 
@@ -33,29 +24,9 @@ class SafeOverviewFragment : BaseFragment<FragmentSafeOverviewBinding>() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.addSafeButton.setOnClickListener {
-            if (viewModel.getTermsAgreed()) {
+            viewModel.checkTerms(this.requireContext()) {
                 findNavController().navigate(SafeOverviewFragmentDirections.actionSafeOverviewFragmentToAddSafeNav())
-            } else {
-                termsBottomSheetDialog = TermsBottomSheetDialog(this.requireContext()).apply {
-                    setContentView(layoutInflater.inflate(R.layout.bottom_sheet_terms_and_conditions, null))
-                }
-                termsBottomSheetDialog.bottom_sheet_terms_and_conditions_third_bullet_text.apply {
-                    text = Html.fromHtml(getString(R.string.terms_third_bullet))
-                    movementMethod = LinkMovementMethod.getInstance()
-                }
-                termsBottomSheetDialog.bottom_sheet_terms_and_conditions_agree.setOnClickListener {
-                    viewModel.setTermsAgreed(true)
-                    termsBottomSheetDialog.dismiss()
-                    findNavController().navigate(SafeOverviewFragmentDirections.actionSafeOverviewFragmentToAddSafeNav())
-                }
-                termsBottomSheetDialog.bottom_sheet_terms_and_conditions_reject.setOnClickListener {
-                    viewModel.setTermsAgreed(false)
-                    termsBottomSheetDialog.dismiss()
-                }
-                termsBottomSheetDialog.show()
             }
         }
     }
-
-    class TermsBottomSheetDialog(context: Context) : BottomSheetDialog(context)
 }
--- app/src/main/java/io/gnosis/safe/ui/safe/overview/SafeOverviewViewModel.kt
+++ app/src/main/java/io/gnosis/safe/ui/safe/overview/SafeOverviewViewModel.kt
@@ -1,12 +1,12 @@
 package io.gnosis.safe.ui.safe.overview
 
 import android.content.Context
-import androidx.lifecycle.LiveData
+import android.text.Html
+import android.text.method.LinkMovementMethod
 import androidx.lifecycle.ViewModel
 import com.google.android.material.bottomsheet.BottomSheetDialog
-import io.gnosis.safe.di.Repositories
-import io.gnosis.safe.ui.base.AppDispatchers
-import io.gnosis.safe.ui.base.BaseStateViewModel
+import io.gnosis.safe.R
+import kotlinx.android.synthetic.main.bottom_sheet_terms_and_conditions.*
 import pm.gnosis.svalinn.common.PreferencesManager
 import pm.gnosis.svalinn.common.utils.edit
 import javax.inject.Inject
@@ -14,20 +14,46 @@ import javax.inject.Inject
 class SafeOverviewViewModel
 @Inject
 constructor(
-    val preferencesManager: PreferencesManager
+    private val preferencesManager: PreferencesManager
 ) : ViewModel() {
 
-    fun setTermsAgreed(value: Boolean) {
+    private lateinit var termsBottomSheetDialog: TermsBottomSheetDialog
+
+    private fun setTermsAgreed(value: Boolean) {
         preferencesManager.prefs.edit {
             putBoolean(TERMS_AGREED, value)
         }
     }
 
-    fun getTermsAgreed(): Boolean = preferencesManager.prefs.getBoolean(TERMS_AGREED, false)
+    private fun getTermsAgreed(): Boolean = preferencesManager.prefs.getBoolean(TERMS_AGREED, false)
+
+    fun checkTerms(context: Context, agreed: () -> Unit) {
+        if (getTermsAgreed()) {
+            agreed()
+        } else {
+            termsBottomSheetDialog = TermsBottomSheetDialog(context).apply {
+                setContentView(layoutInflater.inflate(R.layout.bottom_sheet_terms_and_conditions, null))
+            }
+            termsBottomSheetDialog.bottom_sheet_terms_and_conditions_third_bullet_text.apply {
+                text = Html.fromHtml(context.getString(R.string.terms_third_bullet))
+                movementMethod = LinkMovementMethod.getInstance()
+            }
+            termsBottomSheetDialog.bottom_sheet_terms_and_conditions_agree.setOnClickListener {
+                setTermsAgreed(true)
+                termsBottomSheetDialog.dismiss()
+                agreed()
+            }
+            termsBottomSheetDialog.bottom_sheet_terms_and_conditions_reject.setOnClickListener {
+                setTermsAgreed(false)
+                termsBottomSheetDialog.dismiss()
+            }
+            termsBottomSheetDialog.show()
+        }
+    }
 
     companion object {
         private const val TERMS_AGREED = "prefs.boolean.terms_agreed"
     }
-    class TermsBottomSheetDialog(context: Context) : BottomSheetDialog(context)
 
+    class TermsBottomSheetDialog(context: Context) : BottomSheetDialog(context)
 }
