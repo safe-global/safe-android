86dfa28c55b104658390d1edd267bf0791e41d23
Jose Pablo Alvarez Lacasia <jose.alvarez@gnosis.io>
2020-05-13 10:30:43 +0200

    Added commented out code for the Token Logo loading method

--- app/src/main/java/io/gnosis/safe/utils/ImageUtils.kt
+++ app/src/main/java/io/gnosis/safe/utils/ImageUtils.kt
@@ -1,8 +1,10 @@
 package io.gnosis.safe.utils
 
+import android.graphics.*
 import android.widget.ImageView
 import androidx.core.view.setPadding
 import com.squareup.picasso.Picasso
+import com.squareup.picasso.Transformation
 import io.gnosis.safe.R
 
 fun ImageView.loadTokenLogo(icon: String?) {
@@ -14,18 +16,46 @@ fun ImageView.loadTokenLogo(icon: String?) {
         icon == "local::ethereum" -> {
             setImageResource(R.drawable.ic_ethereum_logo)
         }
-        icon?.startsWith("local::") == true -> {
-//            setImageResource(R.drawable.circle_background)
-        }
+        icon?.startsWith("local::") == true -> setImageResource(R.drawable.background_circle)
+
         !icon.isNullOrBlank() ->
             Picasso.get()
                 .load(icon)
-//                .placeholder(R.drawable.circle_background)
-//                .error(R.drawable.circle_background)
-//                .transform(CircleTransformation)
+                .placeholder(R.drawable.background_circle)
+                .error(R.drawable.background_circle)
+                .transform(CircleTransformation)
                 .into(this)
-        else -> {
-        }
-//            setImageResource(R.drawable.circle_background)
+        else -> setImageResource(R.drawable.background_circle)
     }
+}
+
+object CircleTransformation : Transformation {
+    override fun key() = "Circle Transformation"
+
+    override fun transform(source: Bitmap?): Bitmap? {
+        source ?: return null
+        val size = source.width.coerceAtMost(source.height)
+
+        val x = (source.width - size) / 2
+        val y = (source.height - size) / 2
+
+        val squaredBitmap = Bitmap.createBitmap(source, x, y, size, size)
+        if (squaredBitmap != source) {
+            source.recycle()
+        }
+
+        val bitmap = Bitmap.createBitmap(size, size, source.config)
+
+        val canvas = Canvas(bitmap)
+        val paint = Paint()
+        paint.shader = BitmapShader(squaredBitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP)
+        paint.isAntiAlias = true
+
+        val r = size / 2f
+        canvas.drawCircle(r, r, r, paint)
+
+        squaredBitmap.recycle()
+        return bitmap
+    }
+
 }
