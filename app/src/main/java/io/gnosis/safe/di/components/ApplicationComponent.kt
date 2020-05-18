45bb7ab82785ca94fcc9428dfa7e8fa1e67b1f96
Jose Pablo Alvarez Lacasia <jose.alvarez@gnosis.io>
2020-05-18 15:44:57 +0200

    Removed repositories from the AppComponent interface

--- app/src/main/java/io/gnosis/safe/di/components/ApplicationComponent.kt
+++ app/src/main/java/io/gnosis/safe/di/components/ApplicationComponent.kt
@@ -4,9 +4,6 @@ import android.app.Application
 import android.content.Context
 import androidx.lifecycle.ViewModelProvider
 import dagger.Component
-import io.gnosis.data.repositories.EnsRepository
-import io.gnosis.data.repositories.SafeRepository
-import io.gnosis.data.repositories.TokenRepository
 import io.gnosis.safe.Tracker
 import io.gnosis.safe.di.ApplicationContext
 import io.gnosis.safe.di.modules.*
@@ -41,12 +38,6 @@ interface ApplicationComponent {
     fun termsChecker(): TermsChecker
 
     fun tracker(): Tracker
-
-    fun safeRepository(): SafeRepository
-
-    fun ensRepository(): EnsRepository
-
-    fun tokenRepository(): TokenRepository
 
     // Base injects
     fun inject(activity: BaseActivity)
