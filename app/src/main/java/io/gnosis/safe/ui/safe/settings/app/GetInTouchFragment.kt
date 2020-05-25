47ae9bdfbe68a126f5884021d2757664e49928b0
Vitaly Katz <vitaly@gnosis.pm>
2020-05-25 12:52:25 +0200

    PR comments

--- app/src/main/java/io/gnosis/safe/ui/safe/settings/app/GetInTouchFragment.kt
+++ app/src/main/java/io/gnosis/safe/ui/safe/settings/app/GetInTouchFragment.kt
@@ -1,6 +1,5 @@
 package io.gnosis.safe.ui.safe.settings.app
 
-import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
@@ -73,17 +72,7 @@ class GetInTouchFragment : BaseFragment<FragmentGetInTouchBinding>() {
     }
 
     private fun openTwitter() {
-        val intent = try {
-            // get the Twitter app if possible
-            requireContext().packageManager.getPackageInfo("com.twitter.android", 0)
-            Intent(Intent.ACTION_VIEW, Uri.parse("twitter://user?screen_name=${getString(R.string.id_twitter)}")).apply {
-                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            }
-        } catch (e: ActivityNotFoundException) {
-            // no Twitter app, revert to browser
-            Intent(Intent.ACTION_VIEW, Uri.parse("https://twitter.com/${getString(R.string.id_twitter)}"))
-        }
-        startActivity(intent)
+        startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://twitter.com/${getString(R.string.id_twitter)}")))
     }
 
     private fun openHelpCenter() {
@@ -96,12 +85,15 @@ class GetInTouchFragment : BaseFragment<FragmentGetInTouchBinding>() {
 
     // Do we need telegram channel or is discord sufficient?
     private fun openTelegramChannel() {
-        var intent = try {
-            Intent(Intent.ACTION_VIEW, Uri.parse("tg://resolve?domain=${getString(R.string.id_telegram)}"))
-        } catch (e: ActivityNotFoundException) { //App not found open in browser
-            Intent(Intent.ACTION_VIEW, Uri.parse("http://www.telegram.me/${getString(R.string.id_telegram)}"))
+        kotlin.runCatching {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("tg://resolve?domain=${getString(R.string.id_telegram)}"))
+            startActivity(intent)
         }
-        startActivity(intent)
+            .onFailure {
+                Timber.e(it)
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.telegram.me/${getString(R.string.id_telegram)}"))
+                startActivity(intent)
+            }
     }
 
     private fun sendEmail() {
