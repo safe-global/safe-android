b3b9a233eb70b313832aa5160b36dd4434d632c0
Jose Pablo Alvarez Lacasia <jose.alvarez@gnosis.io>
2020-06-04 10:21:56 +0200

    Added default values to transaction dto and added test for history ETH transfer happy path

--- data/src/test/java/io/gnosis/data/repositories/TransactionRepositoryTest.kt
+++ data/src/test/java/io/gnosis/data/repositories/TransactionRepositoryTest.kt
@@ -1,12 +1,15 @@
 package io.gnosis.data.repositories
 
 import io.gnosis.data.backend.TransactionServiceApi
+import io.gnosis.data.models.*
+import io.gnosis.data.repositories.TokenRepository.Companion.ETH_TOKEN_INFO
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.mockk
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Assert.*
 import org.junit.Test
+import pm.gnosis.crypto.utils.asEthereumAddressChecksumString
 import pm.gnosis.model.Solidity
 import pm.gnosis.utils.asEthereumAddressString
 import java.math.BigInteger
@@ -33,6 +36,40 @@ class TransactionRepositoryTest {
     }
 
     @Test
-    fun `loadTransaction (valid address) should returned paged mapped list`() {
+    fun `loadTransaction (valid address, single history eth transfer) should returned paged mapped list`() = runBlockingTest {
+        val transactionDto = buildTransactionDto().copy(transfers = listOf(buildTransferDto()))
+        val safeAddress = Solidity.Address(BigInteger.ONE)
+        val pagedResult = listOf(transactionDto)
+        coEvery { transactionServiceApi.loadTransactions(any()) } returns Page(1, null, null, pagedResult)
+
+        val actual = transactionRepository.getTransactions(safeAddress)
+
+        assertEquals(actual.results.size, 1)
+        assert(actual.results[0] is Transaction.Transfer)
+        (actual.results[0] as Transaction.Transfer).let { transfer ->
+            assertEquals(transactionDto.transfers?.get(0)?.to, transfer.recipient)
+            assertEquals(transactionDto.transfers?.get(0)?.from, transfer.sender)
+            assertEquals(transactionDto.transfers?.get(0)?.value, transfer.value)
+            assertEquals(TokenRepository.ETH_SERVICE_TOKEN_INFO, transfer.tokenInfo)
+        }
+        coVerify(exactly = 1) { transactionServiceApi.loadTransactions(safeAddress.asEthereumAddressChecksumString()) }
     }
+
+    private fun buildTransactionDto(): TransactionDto =
+        TransactionDto(
+            to = Solidity.Address(BigInteger.ONE),
+            value = BigInteger.ONE,
+            safeTxGas = BigInteger.ONE,
+            baseGas = BigInteger.ONE,
+            gasPrice = BigInteger.ONE,
+            txType = TransactionType.ETHEREUM_TRANSACTION
+        )
+
+    private fun buildTransferDto(): TransferDto =
+        TransferDto(
+            to = Solidity.Address(BigInteger.ONE),
+            from = Solidity.Address(BigInteger.ONE),
+            type = TransferType.ETHER_TRANSFER,
+            value = BigInteger.ONE
+        )
 }
