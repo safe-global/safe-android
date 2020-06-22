92cb60ca7dc8a70be00abe2d65e71a14dc333c95
Jose Pablo Alvarez Lacasia <jose.alvarez@gnosis.io>
2020-06-22 14:56:28 +0200

    Added retry logic and dialog when scanning something that is not an ethereum address

--- qrscanner/src/main/java/io/gnosis/safe/qrscanner/QRCodeScanActivity.kt
+++ qrscanner/src/main/java/io/gnosis/safe/qrscanner/QRCodeScanActivity.kt
@@ -5,9 +5,12 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.screen_scan.*
+import pm.gnosis.svalinn.utils.ethereum.ERC67Parser
+import pm.gnosis.utils.asEthereumAddress
 
 /*
  * Check https://github.com/walleth/walleth/tree/master/app/src/main/java/org/walleth/activities/qrscan
@@ -46,10 +49,21 @@ class QRCodeScanActivity : AppCompatActivity() {
     }
 
     private fun finishWithResult(value: String) {
-        val result = Intent().apply { putExtra(RESULT_EXTRA, value) }
-        setResult(Activity.RESULT_OK, result)
-        finish()
+        if (isEthereumAddress(value)) {
+            val result = Intent().apply { putExtra(RESULT_EXTRA, value) }
+            setResult(Activity.RESULT_OK, result)
+            finish()
+        } else {
+            AlertDialog.Builder(this)
+                .setTitle(R.string.error_title)
+                .setMessage(R.string.error_message)
+                .setNegativeButton(R.string.retry_button) { _, _ -> videographer.open(scan_view_finder) }
+                .create().show()
+        }
     }
+
+    private fun isEthereumAddress(result: String): Boolean =
+        (result.asEthereumAddress() ?: ERC67Parser.parse(result)?.address) != null
 
     companion object {
         const val RESULT_EXTRA = "extra.string.scan_result"
