9e5a7fd73de5e5802f8069001fe4040a3b724a80
Jose Pablo Alvarez Lacasia <jose.alvarez@gnosis.io>
2020-07-08 16:43:22 +0200

    Added basic styling and view components to layout

--- app/src/main/java/io/gnosis/safe/ui/safe/settings/safe/SafeSettingsFragment.kt
+++ app/src/main/java/io/gnosis/safe/ui/safe/settings/safe/SafeSettingsFragment.kt
@@ -26,7 +26,7 @@ class SafeSettingsFragment : BaseViewBindingFragment<FragmentSettingsSafeBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.button.setOnClickListener {
+        binding.remove.setOnClickListener {
             viewModel.removeSafe()
         }
     }
