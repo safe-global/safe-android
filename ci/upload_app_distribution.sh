b6bc97c656513c49a365d28d7630cb78dc29af67
Dirk Jäckel <dirk@novoda.com>
2020-07-10 20:53:38 +0200

    upload_app_distribution parametrized

--- ci/upload_app_distribution.sh
+++ ci/upload_app_distribution.sh
@@ -5,7 +5,7 @@ set -e
 # Buildkite uses a clean state for each step (for concurrency)
 source ./ci/prepare_env_buildkite.sh
 
-export FIREBASE_GROUP_RINKEBY="rinkeby-release"
+export FIREBASE_GROUP="$1"
 
 # Buildkite branch equals to tag name if build was triggered by tag
 if [[ $BUILKITE_BRANCH  =~ ^v[0-9]+.* ]]; then
@@ -15,5 +15,5 @@ fi
 echo "INFURA_API_KEY=$INFURA_API_KEY" > project_keys
 
 #TODO: requires app distribution setup in firebase
-./gradlew assembleRinkeby
-./gradlew appDistributionUploadRinkeby
+./gradlew assemble${2^}
+./gradlew appDistributionUpload${2^}
