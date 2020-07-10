b432f3ee1bc09a7784b551af5502918df5ee74d9
Dirk Jäckel <dirk.jaeckel@gnosis.pm>
2020-07-10 16:06:05 +0200

    More typos d'oh

--- ci/upload_app_distribution_rinkeby.sh
+++ ci/upload_app_distribution_rinkeby.sh
@@ -5,7 +5,7 @@ set -e
 # Buildkite uses a clean state for each step (for concurrency)
 source ./ci/prepare_env_buildkite.sh
 
-export FIREBASE_GROUP_MAINNET="mainnet-release"
+export FIREBASE_GROUP_RINKEBY="rinkeby-release"
 
 # Buildkite branch equals to tag name if build was triggered by tag
 if [[ $BUILKITE_BRANCH  =~ ^v[0-9]+.* ]]; then
