1950022b33e485f3d61d50c0afee19f0893147e6
Jose Pablo Alvarez Lacasia <jose.alvarez@gnosis.io>
2020-07-13 16:33:01 +0200

    Attached safe details to the layout

--- app/src/main/java/io/gnosis/safe/ui/safe/share/ShareSafeDialog.kt
+++ app/src/main/java/io/gnosis/safe/ui/safe/share/ShareSafeDialog.kt
@@ -4,39 +4,76 @@ import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
+import androidx.lifecycle.Observer
+import com.google.android.material.snackbar.Snackbar
 import io.gnosis.safe.HeimdallApplication
 import io.gnosis.safe.R
+import io.gnosis.safe.ScreenId
+import io.gnosis.safe.databinding.DialogShareSafeBinding
 import io.gnosis.safe.di.components.DaggerViewComponent
+import io.gnosis.safe.di.components.ViewComponent
 import io.gnosis.safe.di.modules.ViewModule
+import io.gnosis.safe.ui.base.BaseStateViewModel
+import io.gnosis.safe.ui.base.BaseViewBindingDialogFragment
+import io.gnosis.safe.utils.formatEthAddress
+import pm.gnosis.svalinn.common.utils.snackbar
+import pm.gnosis.svalinn.common.utils.visible
+import timber.log.Timber
 import javax.inject.Inject
 
-class ShareSafeDialog : DialogFragment() {
+class ShareSafeDialog : BaseViewBindingDialogFragment<DialogShareSafeBinding>() {
 
     @Inject
     lateinit var viewModel: ShareSafeViewModel
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        inject()
+    override fun inflateBinding(inflater: LayoutInflater, container: ViewGroup?): DialogShareSafeBinding =
+        DialogShareSafeBinding.inflate(layoutInflater)
+
+    override fun screenId(): ScreenId? = null
+
+    override fun inject(component: ViewComponent) {
+        component.inject(this)
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        setStyle(STYLE_NO_FRAME, 0)
-        super.onCreate(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.state.observe(viewLifecycleOwner, Observer {
+            when (val viewAction = it.viewAction) {
+                is BaseStateViewModel.ViewAction.Loading -> toggleLoading(viewAction.isLoading)
+                is ShowSafeDetails -> loadSafeDetails(viewAction.safeDetails)
+                is BaseStateViewModel.ViewAction.ShowError -> showError(viewAction.error)
+            }
+        })
+        viewModel.load()
     }
 
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val view = LayoutInflater.from(requireContext()).inflate(R.layout.dialog_share_safe, null)
-        return AlertDialog.Builder(requireContext()).setView(view).create()
+    private fun loadSafeDetails(safeDetails: SafeDetails) {
+        toggleLoading(false)
+        with(binding) {
+            blockies.setAddress(safeDetails.safe.address)
+            safeLocalName.text = safeDetails.safe.localName
+            safeAddress.text = safeDetails.safe.address.formatEthAddress(requireContext())
+            safeEnsName.text = safeDetails.ensName ?: getString(R.string.safe_settings_not_set)
+        }
     }
 
-    private fun inject() {
-        DaggerViewComponent.builder()
-            .viewModule(ViewModule(requireContext()))
-            .applicationComponent(HeimdallApplication[requireContext()])
-            .build()
-            .inject(this)
+    private fun toggleLoading(loading: Boolean) {
+        with(binding) {
+            progress.visible(loading)
+            safeFields.visible(!loading)
+        }
+    }
+
+    private fun showError(error: Throwable) {
+        with(binding) {
+            progress.visible(false)
+            safeFields.visible(false)
+        }
+        Timber.e(error)
+        snackbar(requireView(), Snackbar.LENGTH_LONG, R.string.error_invalid_safe)
     }
 }
--- app/src/main/java/io/gnosis/safe/ui/safe/share/ShareSafeViewModel.kt
+++ app/src/main/java/io/gnosis/safe/ui/safe/share/ShareSafeViewModel.kt
@@ -1,17 +1,45 @@
 package io.gnosis.safe.ui.safe.share
 
+import io.gnosis.data.models.Safe
+import io.gnosis.data.repositories.EnsRepository
+import io.gnosis.data.repositories.SafeRepository
 import io.gnosis.safe.ui.base.AppDispatchers
 import io.gnosis.safe.ui.base.BaseStateViewModel
+import timber.log.Timber
 import javax.inject.Inject
 
 class ShareSafeViewModel
 @Inject constructor(
+    private val safeRepository: SafeRepository,
+    private val ensRepository: EnsRepository,
     appDispatchers: AppDispatchers
 ) : BaseStateViewModel<ShareSafeState>(appDispatchers) {
 
     override fun initialState(): ShareSafeState = ShareSafeState()
+
+    fun load() {
+        safeLaunch {
+            safeRepository.getActiveSafe()?.let { activeSafe ->
+                val ensName = runCatching { ensRepository.reverseResolve(activeSafe.address) }
+                    .onFailure { Timber.e(it) }
+                    .getOrNull()
+                updateState {
+                    ShareSafeState(ShowSafeDetails(SafeDetails(activeSafe, ensName)))
+                }
+            }
+        }
+    }
 }
 
 data class ShareSafeState(
     override var viewAction: BaseStateViewModel.ViewAction? = BaseStateViewModel.ViewAction.Loading(true)
 ) : BaseStateViewModel.State
+
+data class SafeDetails(
+    val safe: Safe,
+    val ensName: String?
+)
+
+data class ShowSafeDetails(
+    val safeDetails: SafeDetails
+) : BaseStateViewModel.ViewAction
