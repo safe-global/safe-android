0c98ad902551df6f3fc7b58d939194d0d0b6871f
Vitaly Katz <vitaly@gnosis.pm>
2020-07-24 08:39:59 +0200

    Add tests for editing local safe name

---
+++ app/src/test/java/io/gnosis/safe/ui/settings/safe/SafeSettingsEditNameViewModelTest.kt
@@ -0,0 +1,62 @@
+package io.gnosis.safe.ui.settings.safe
+
+import io.gnosis.data.models.Safe
+import io.gnosis.data.repositories.SafeRepository
+import io.gnosis.safe.MainCoroutineScopeRule
+import io.gnosis.safe.TestLifecycleRule
+import io.gnosis.safe.TestLiveDataObserver
+import io.gnosis.safe.appDispatchers
+import io.gnosis.safe.ui.base.BaseStateViewModel
+import io.mockk.*
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Rule
+import org.junit.Test
+import pm.gnosis.model.Solidity
+import java.math.BigInteger
+
+class SafeSettingsEditNameViewModelTest {
+
+    @get:Rule
+    val coroutineScope = MainCoroutineScopeRule()
+
+    @get:Rule
+    val instantExecutorRule = TestLifecycleRule()
+
+    private val safeRepository = mockk<SafeRepository>(relaxed = true)
+    private lateinit var viewModel: SafeSettingsEditNameViewModel
+
+    @Test
+    fun `saveLocalName (name) - should change local name for safe`() = runBlockingTest {
+
+        coEvery { safeRepository.activeSafeFlow() } returnsMany listOf(
+            flow {
+                emit(SAFE_1)
+            },
+            flow {
+                emit(SAFE_2)
+            }
+        )
+        coEvery { safeRepository.getActiveSafe() } returnsMany listOf(SAFE_1, SAFE_2)
+        coEvery { safeRepository.saveSafe(any()) } just Runs
+
+        val stateObserver = TestLiveDataObserver<BaseStateViewModel.State>()
+        viewModel = SafeSettingsEditNameViewModel(safeRepository, appDispatchers)
+        viewModel.state.observeForever(stateObserver)
+
+        with(stateObserver.values()[0] as EditNameState) {
+            assert(name == "safe1" && viewAction is BaseStateViewModel.ViewAction.None)
+        }
+
+        viewModel.saveLocalName("safe2")
+
+        with(stateObserver.values()[0] as EditNameState) {
+            assert(name == "safe2" && viewAction is BaseStateViewModel.ViewAction.CloseScreen)
+        }
+    }
+
+    companion object {
+        private val SAFE_1 = Safe(Solidity.Address(BigInteger.ZERO), "safe1")
+        private val SAFE_2 = Safe(Solidity.Address(BigInteger.ZERO), "safe2")
+    }
+}
