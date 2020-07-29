3cc073b73272e8fb98e881e6b8fcf690e9a739a0
Dirk Jäckel <dirk.jaeckel@gnosis.pm>
2020-07-29 18:47:53 +0200

    Adds missing settingsname for queued settings changes

--- app/src/main/java/io/gnosis/safe/ui/transactions/TransactionListViewHolderFactory.kt
+++ app/src/main/java/io/gnosis/safe/ui/transactions/TransactionListViewHolderFactory.kt
@@ -159,6 +159,8 @@ class SettingsChangeQueuedViewHolder(private val viewBinding: ItemTxQueuedSettin
             status.setTextColor(ResourcesCompat.getColor(resources, viewTransfer.statusColorRes, theme))
 
             dateTime.text = viewTransfer.dateTimeText
+            settingName.text = viewTransfer.settingNameText
+
             confirmations.setTextColor(ResourcesCompat.getColor(resources, viewTransfer.confirmationsTextColor, theme))
             confirmationsIcon.setImageDrawable(ResourcesCompat.getDrawable(resources, viewTransfer.confirmationsIcon, theme))
             confirmations.text = resources.getString(R.string.tx_list_confirmations, viewTransfer.confirmations, viewTransfer.threshold)
