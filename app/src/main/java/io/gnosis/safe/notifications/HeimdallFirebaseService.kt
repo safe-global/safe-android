b90a0d14281eba563d31e3f332c39f5b61b7ff27
Vitaly Katz <vitaly@gnosis.pm>
2020-08-05 10:47:34 +0200

    Add firebase messaging service

---
+++ app/src/main/java/io/gnosis/safe/notifications/HeimdallFirebaseService.kt
@@ -0,0 +1,39 @@
+package io.gnosis.safe.notifications
+
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import io.gnosis.safe.HeimdallApplication
+import timber.log.Timber
+import javax.inject.Inject
+
+class HeimdallFirebaseService : FirebaseMessagingService() {
+
+    @Inject
+    lateinit var notificationRepo: NotificationRepository
+
+
+    override fun onCreate() {
+        super.onCreate()
+        HeimdallApplication[this].inject(this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        // No data received
+        if (message.data.isEmpty()) return
+
+        try {
+            //notificationRepo.handleNotification()
+        } catch (e: IllegalArgumentException) {
+            Timber.e(e)
+        }
+    }
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+        Timber.d("Firebase token: $token")
+    }
+}
