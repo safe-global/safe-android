3bb599fb01ea8fae1bde73aa90af1b0a6188895e
Dirk Jäckel <dirk.jaeckel@gnosis.pm>
2020-08-07 13:19:56 +0200

    Introducing parametrised test over direction

--- data/src/test/java/io/gnosis/data/repositories/TransactionRepositoryTest.kt
+++ data/src/test/java/io/gnosis/data/repositories/TransactionRepositoryTest.kt
@@ -28,19 +28,69 @@ import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
 import pm.gnosis.crypto.utils.asEthereumAddressChecksumString
 import pm.gnosis.model.Solidity
 import pm.gnosis.utils.asDecimalString
 import pm.gnosis.utils.asEthereumAddress
 
-class TransactionRepositoryTest {
-
+@RunWith(Parameterized::class)
+class TransactionRepositoryTransferTest(
+    private val direction: TransactionDirection
+) {
     private val gatewayApi = mockk<GatewayApi>()
 
     private val transactionRepository = TransactionRepository(gatewayApi)
     private val defaultSafeAddress = "0x1C8b9B78e3085866521FE206fa4c1a67F49f153A".asEthereumAddress()!!
-    private val defaultFromAddress = "0x7cd310A8AeBf268bF78ea16C601F201ca81e84Cc".asEthereumAddress()!!
-    private val defaultToAddress = "0x2134Bb3DE97813678daC21575E7A77a95079FC51".asEthereumAddress()!!
+
+    companion object {
+        @JvmStatic
+        @Parameterized.Parameters()
+        fun directions(): Array<TransactionDirection> {
+            return TransactionDirection.values()
+        }
+    }
+
+    @Test
+    fun `getTransactions (all transfer types) should return Transfers`() = runBlockingTest {
+        val pagedResult = listOf(
+            buildGateTransactionDto(txInfo = buildTransferTxInfo(direction = direction, transferInfo = buildTransferInfoERC20())),
+            buildGateTransactionDto(txInfo = buildTransferTxInfo(direction = direction, transferInfo = buildTransferInfoEther())),
+            buildGateTransactionDto(txInfo = buildTransferTxInfo(direction = direction, transferInfo = buildTransferInfoERC721()))
+        )
+        coEvery { gatewayApi.loadTransactions(any()) } returns Page(1, null, null, pagedResult)
+
+        val actual = transactionRepository.getTransactions(defaultSafeAddress)
+
+        assertEquals(3, actual.results.size)
+        (0..2).forEach { i ->
+            with(actual.results[i] as Transaction.Transfer) {
+                assertEquals(pagedResult[i].executionInfo?.nonce, this.nonce)
+                when ((pagedResult[i].txInfo as Transfer).transferInfo) {
+                    is Erc20Transfer -> {
+                        assertEquals(ServiceTokenInfo.TokenType.ERC20, this.tokenInfo?.type)
+                        assertEquals(((pagedResult[i].txInfo as Transfer).transferInfo as Erc20Transfer).value, value.asDecimalString())
+                    }
+                    is Erc721Transfer -> {
+                        assertEquals(ServiceTokenInfo.TokenType.ERC721, this.tokenInfo?.type)
+                        assertEquals(1.toBigInteger(), value)
+                    }
+                    is EtherTransfer -> {
+                        assertEquals(null, this.tokenInfo?.type)
+                        assertEquals(((pagedResult[i].txInfo as Transfer).transferInfo as EtherTransfer).value, value.asDecimalString())
+                    }
+                }
+            }
+        }
+    }
+}
+
+class TransactionRepositoryTest {
+    private val gatewayApi = mockk<GatewayApi>()
+
+    private val transactionRepository = TransactionRepository(gatewayApi)
+    private val defaultSafeAddress = "0x1C8b9B78e3085866521FE206fa4c1a67F49f153A".asEthereumAddress()!!
 
     @Test
     fun `getTransactions (api failure) should throw`() = runBlockingTest {
@@ -57,7 +107,7 @@ class TransactionRepositoryTest {
     }
 
     @Test
-    fun `getTransactions (all tx type) should return respective tx type`() = runBlockingTest {
+    fun `getTransactions (all tx types) should return respective tx type`() = runBlockingTest {
         val pagedResult = listOf(
             buildGateTransactionDto(txInfo = buildTransferTxInfo()),
             buildGateTransactionDto(txInfo = buildCustomTxInfo()),
@@ -110,7 +160,7 @@ class TransactionRepositoryTest {
                     is Transfer -> {
                         assertEquals(pagedResult[i].executionInfo?.nonce, (this as Transaction.Transfer).nonce)
                         assertEquals(pagedResult[i].txStatus, this.status)
-                        assertEquals((pagedResult[i].txInfo as Transfer).direction != TransactionDirection.OUTGOING, incoming)
+                        assertEquals((pagedResult[i].txInfo as Transfer).direction == TransactionDirection.INCOMING, incoming)
                     }
                     is Custom -> {
                         assertEquals(pagedResult[i].executionInfo?.nonce, (this as Transaction.Custom).nonce)
@@ -121,39 +171,6 @@ class TransactionRepositoryTest {
                         assertEquals(pagedResult[i].executionInfo?.nonce, (this as Transaction.SettingsChange).nonce)
                         assertEquals(pagedResult[i].txStatus, this.status)
                         assertEquals((pagedResult[i].txInfo as SettingsChange).dataDecoded, dataDecoded)
-                    }
-                }
-            }
-        }
-    }
-
-    @Test
-    fun `getTransactions (all transfer types) should return Transfers`() = runBlockingTest {
-        val pagedResult = listOf(
-            buildGateTransactionDto(txInfo = buildTransferTxInfo(transferInfo = buildTransferInfoERC20())),
-            buildGateTransactionDto(txInfo = buildTransferTxInfo(transferInfo = buildTransferInfoEther())),
-            buildGateTransactionDto(txInfo = buildTransferTxInfo(transferInfo = buildTransferInfoERC721()))
-        )
-        coEvery { gatewayApi.loadTransactions(any()) } returns Page(1, null, null, pagedResult)
-
-        val actual = transactionRepository.getTransactions(defaultSafeAddress)
-
-        assertEquals(3, actual.results.size)
-        (0..2).forEach { i ->
-            with(actual.results[i] as Transaction.Transfer) {
-                assertEquals(pagedResult[i].executionInfo?.nonce, this.nonce)
-                when ((pagedResult[i].txInfo as Transfer).transferInfo) {
-                    is Erc20Transfer -> {
-                        assertEquals(ServiceTokenInfo.TokenType.ERC20, this.tokenInfo?.type)
-                        assertEquals(((pagedResult[i].txInfo as Transfer).transferInfo as Erc20Transfer).value, value.asDecimalString())
-                    }
-                    is Erc721Transfer -> {
-                        assertEquals(ServiceTokenInfo.TokenType.ERC721, this.tokenInfo?.type)
-                        assertEquals(1.toBigInteger(), value)
-                    }
-                    is EtherTransfer -> {
-                        assertEquals(null, this.tokenInfo?.type)
-                        assertEquals(((pagedResult[i].txInfo as Transfer).transferInfo as EtherTransfer).value, value.asDecimalString())
                     }
                 }
             }
@@ -253,96 +270,96 @@ class TransactionRepositoryTest {
 
         assertEquals(null, result)
     }
-
-    private fun buildGateTransactionDto(
-        id: String = "1234",
-        status: TransactionStatus = TransactionStatus.SUCCESS,
-        txInfo: TransactionInfo = buildTransferTxInfo()
-    ): GateTransactionDto =
-        GateTransactionDto(
-            id = id,
-            txStatus = status,
-            txInfo = txInfo,
-            executionInfo = ExecutionInfo(
-                nonce = 1.toBigInteger(),
-                confirmationsSubmitted = 2,
-                confirmationsRequired = 2
-            ),
-            timestamp = 1L
-        )
-
-    private fun buildCustomTxInfo(
-        value: String = "1",
-        to: Solidity.Address = "0x1234".asEthereumAddress()!!,
-        dataSize: String = "96"
-    ): Custom =
-        Custom(
-            type = GateTransactionType.SettingsChange,
-            value = value,
-            to = to,
-            dataSize = dataSize
-        )
-
-    private fun buildSettingsChangeTxInfo(
-        dataDecoded: DataDecodedDto = DataDecodedDto("defaultMethod", listOf())
-    ): SettingsChange =
-        SettingsChange(
-            type = GateTransactionType.SettingsChange,
-            dataDecoded = dataDecoded
-        )
-
-    private fun buildTransferTxInfo(
-        sender: Solidity.Address = defaultFromAddress,
-        recipient: Solidity.Address = defaultToAddress,
-        direction: TransactionDirection = TransactionDirection.OUTGOING,
-        transferInfo: TransferInfo = buildTransferInfoERC20()
-    ): Transfer =
-        Transfer(
-            type = GateTransactionType.Transfer,
-            sender = sender,
-            recipient = recipient,
-            direction = direction,
-            transferInfo = transferInfo
-        )
-
-    private fun buildTransferInfoERC20(
-        value: String = "10000000",
-        symbol: String = "WETH",
-        name: String = "Wrapped Ether",
-        address: Solidity.Address = "0x1234".asEthereumAddress()!!,
-        uri: String = "https://www.erc20",
-        decimals: Int? = 18
-    ): TransferInfo =
-        Erc20Transfer(
-            type = GateTransferType.ERC20,
-            value = value,
-            tokenSymbol = symbol,
-            tokenName = name,
-            tokenAddress = address,
-            logoUri = uri,
-            decimals = decimals
-        )
-
-    private fun buildTransferInfoERC721(
-        symbol: String = "CK",
-        name: String = "Crypto Kitties",
-        address: Solidity.Address = "0x123456".asEthereumAddress()!!,
-        uri: String = "https://www.erc721",
-        id: String = "id"
-    ): TransferInfo =
-        Erc721Transfer(
-            type = GateTransferType.ERC721,
-            tokenSymbol = symbol,
-            tokenName = name,
-            tokenAddress = address,
-            logoUri = uri,
-            tokenId = id
-        )
-
-    private fun buildTransferInfoEther(
-        value: String = "0"
-    ): TransferInfo = EtherTransfer(
-        type = GateTransferType.ETHER,
-        value = value
-    )
 }
+
+private fun buildGateTransactionDto(
+    id: String = "1234",
+    status: TransactionStatus = TransactionStatus.SUCCESS,
+    txInfo: TransactionInfo = buildTransferTxInfo()
+): GateTransactionDto =
+    GateTransactionDto(
+        id = id,
+        txStatus = status,
+        txInfo = txInfo,
+        executionInfo = ExecutionInfo(
+            nonce = 1.toBigInteger(),
+            confirmationsSubmitted = 2,
+            confirmationsRequired = 2
+        ),
+        timestamp = 1L
+    )
+
+private fun buildCustomTxInfo(
+    value: String = "1",
+    to: Solidity.Address = "0x1234".asEthereumAddress()!!,
+    dataSize: String = "96"
+): Custom =
+    Custom(
+        type = GateTransactionType.SettingsChange,
+        value = value,
+        to = to,
+        dataSize = dataSize
+    )
+
+private fun buildSettingsChangeTxInfo(
+    dataDecoded: DataDecodedDto = DataDecodedDto("defaultMethod", listOf())
+): SettingsChange =
+    SettingsChange(
+        type = GateTransactionType.SettingsChange,
+        dataDecoded = dataDecoded
+    )
+
+private fun buildTransferTxInfo(
+    sender: Solidity.Address = "0x7cd310A8AeBf268bF78ea16C601F201ca81e84Cc".asEthereumAddress()!!,
+    recipient: Solidity.Address = "0x2134Bb3DE97813678daC21575E7A77a95079FC51".asEthereumAddress()!!,
+    direction: TransactionDirection = TransactionDirection.OUTGOING,
+    transferInfo: TransferInfo = buildTransferInfoERC20()
+): Transfer =
+    Transfer(
+        type = GateTransactionType.Transfer,
+        sender = sender,
+        recipient = recipient,
+        direction = direction,
+        transferInfo = transferInfo
+    )
+
+private fun buildTransferInfoERC20(
+    value: String = "10000000",
+    symbol: String = "WETH",
+    name: String = "Wrapped Ether",
+    address: Solidity.Address = "0x1234".asEthereumAddress()!!,
+    uri: String = "https://www.erc20",
+    decimals: Int? = 18
+): TransferInfo =
+    Erc20Transfer(
+        type = GateTransferType.ERC20,
+        value = value,
+        tokenSymbol = symbol,
+        tokenName = name,
+        tokenAddress = address,
+        logoUri = uri,
+        decimals = decimals
+    )
+
+private fun buildTransferInfoERC721(
+    symbol: String = "CK",
+    name: String = "Crypto Kitties",
+    address: Solidity.Address = "0x123456".asEthereumAddress()!!,
+    uri: String = "https://www.erc721",
+    id: String = "id"
+): TransferInfo =
+    Erc721Transfer(
+        type = GateTransferType.ERC721,
+        tokenSymbol = symbol,
+        tokenName = name,
+        tokenAddress = address,
+        logoUri = uri,
+        tokenId = id
+    )
+
+private fun buildTransferInfoEther(
+    value: String = "0"
+): TransferInfo = EtherTransfer(
+    type = GateTransferType.ETHER,
+    value = value
+)
