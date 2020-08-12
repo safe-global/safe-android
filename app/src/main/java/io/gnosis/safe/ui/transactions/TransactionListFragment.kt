f5f62e8eb4e6ae3ae8b7f947e665b27534e2def6
Vitaly Katz <vitaly@gnosis.pm>
2020-08-12 14:28:17 +0200

    Rename layout file for assets fragment & add tx hash argument

--- app/src/main/java/io/gnosis/safe/ui/transactions/TransactionListFragment.kt
+++ app/src/main/java/io/gnosis/safe/ui/transactions/TransactionListFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.core.view.isVisible
 import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.navArgs
 import androidx.paging.ExperimentalPagingApi
 import androidx.paging.LoadState
 import androidx.paging.PagingData
@@ -27,6 +28,9 @@ import javax.inject.Inject
 class TransactionListFragment : SafeOverviewBaseFragment<FragmentTransactionListBinding>() {
 
     override fun screenId() = ScreenId.TRANSACTIONS
+
+    private val navArgs by navArgs<TransactionListFragmentArgs>()
+    private val txHash by lazy { navArgs.txHash }
 
     @Inject
     lateinit var viewModel: TransactionListViewModel
