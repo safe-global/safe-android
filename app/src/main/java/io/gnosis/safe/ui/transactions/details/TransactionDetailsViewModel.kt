9cd805c4cc8b884d9148a1a4cd9aea0c9a1cd8d4
Dirk Jäckel <dirk.jaeckel@gnosis.pm>
2020-08-13 16:41:26 +0200

    Remove failure handler. safeLaunch() already logs errors.

--- app/src/main/java/io/gnosis/safe/ui/transactions/details/TransactionDetailsViewModel.kt
+++ app/src/main/java/io/gnosis/safe/ui/transactions/details/TransactionDetailsViewModel.kt
@@ -5,7 +5,6 @@ import io.gnosis.data.repositories.SafeRepository
 import io.gnosis.data.repositories.TransactionRepository
 import io.gnosis.safe.ui.base.AppDispatchers
 import io.gnosis.safe.ui.base.BaseStateViewModel
-import timber.log.Timber
 import javax.inject.Inject
 
 class TransactionDetailsViewModel
@@ -19,14 +18,8 @@ class TransactionDetailsViewModel
 
     fun loadDetails(txId: String) {
         safeLaunch {
-
-            kotlin.runCatching {
-                val txDetails = transactionRepository.getTransactionDetails(txId)
-                updateState { TransactionDetailsViewState(txDetails, ViewAction.Loading(false)) }
-            }.onFailure {
-                // TODO stop spinner and show error state "Data cannot be loaded" Same as for tx list
-                Timber.e(it)
-            }
+            val txDetails = transactionRepository.getTransactionDetails(txId)
+            updateState { TransactionDetailsViewState(txDetails, ViewAction.Loading(false)) }
         }
     }
 }
