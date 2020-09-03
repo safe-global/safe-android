256f1c89b99a150ba143e07fc415ca5ab76f2bf2
Vitaly Katz <vitaly@gnosis.pm>
2020-09-03 15:40:38 +0200

    Fix slack notification

--- ci/notify_slack.sh
+++ ci/notify_slack.sh
@@ -5,8 +5,8 @@ set -e
 [[ -z "${SLACK_WEBHOOK}" ]] && exit 0
 
 repo_url="$(git config --get remote.origin.url)"
-release_path=/releases/tag/${BUILDKITE_BRANCH}
-release_url="${repo_url/.git/$release_path}"
+release_path="releases/tag/${BUILDKITE_BRANCH}"
+release_url="${repo_url}/${release_path}"
 product_name=${BUILDKITE_ORGANIZATION_SLUG}/$(echo ${BUILDKITE_REPO} | rev | cut -d '/' -f 1 | rev)
 curl -X POST -H 'Content-type: application/json' \
 --data "{\"text\":\"<${release_url}|Version ${APP_VERSION_NAME}> for ${product_name} is out!\"}" \
