24466639b32baf4b386ffc522866f476727af801
Vitaly Katz <vitaly@gnosis.pm>
2020-09-09 13:41:06 +0200

    Load NFT logo image

--- app/src/main/java/io/gnosis/safe/ui/assets/collectibles/CollectiblesViewHolderFactory.kt
+++ app/src/main/java/io/gnosis/safe/ui/assets/collectibles/CollectiblesViewHolderFactory.kt
@@ -1,14 +1,27 @@
 package io.gnosis.safe.ui.assets.collectibles
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.core.view.setPadding
 import androidx.viewbinding.ViewBinding
+import com.squareup.picasso.Callback
+import com.squareup.picasso.Picasso
+import com.squareup.picasso.PicassoProvider
+import com.squareup.picasso.Target
+import io.gnosis.safe.R
 import io.gnosis.safe.databinding.ItemCollectibleCollectibleBinding
 import io.gnosis.safe.databinding.ItemCollectibleNftHeaderBinding
 import io.gnosis.safe.ui.base.adapter.Adapter
 import io.gnosis.safe.ui.base.adapter.BaseFactory
 import io.gnosis.safe.ui.base.adapter.UnsupportedViewType
+import io.gnosis.safe.utils.CircleTransformation
+import io.gnosis.safe.utils.dpToPx
+import pm.gnosis.svalinn.common.utils.getColorCompat
 import pm.gnosis.svalinn.common.utils.visible
+import java.lang.Exception
 
 enum class CollectiblesViewType {
     NFT_HEADER,
@@ -25,14 +38,14 @@ class CollectiblesViewHolderFactory : BaseFactory<BaseCollectiblesViewHolder<Col
         } as BaseCollectiblesViewHolder<CollectibleViewData>
 
     override fun layout(layoutInflater: LayoutInflater, parent: ViewGroup, viewType: Int): ViewBinding =
-        when(viewType) {
+        when (viewType) {
             CollectiblesViewType.NFT_HEADER.ordinal -> ItemCollectibleNftHeaderBinding.inflate(layoutInflater, parent, false)
             CollectiblesViewType.COLLECTIBLE_ITEM.ordinal -> ItemCollectibleCollectibleBinding.inflate(layoutInflater, parent, false)
             else -> throw UnsupportedViewType(javaClass.name)
         }
 
     override fun viewTypeFor(item: CollectibleViewData): Int =
-        when(item) {
+        when (item) {
             is CollectibleViewData.NftHeader -> CollectiblesViewType.NFT_HEADER
             is CollectibleViewData.CollectibleItem -> CollectiblesViewType.COLLECTIBLE_ITEM
         }.ordinal
@@ -53,12 +66,53 @@ class NftHeaderViewHolder(private val viewBinding: ItemCollectibleNftHeaderBindi
 }
 
 class CollectibleItemViewHolder(private val viewBinding: ItemCollectibleCollectibleBinding) :
-    BaseCollectiblesViewHolder<CollectibleViewData.CollectibleItem>(viewBinding) {
+    BaseCollectiblesViewHolder<CollectibleViewData.CollectibleItem>(viewBinding), Target {
 
     override fun bind(data: CollectibleViewData.CollectibleItem, payloads: List<Any>) {
         with(viewBinding) {
             name.text = data.collectible.name
             description.text = data.collectible.description
+            logo.loadNftImage(data.collectible.imageUri, this@CollectibleItemViewHolder)
+        }
+    }
+
+    override fun onPrepareLoad(placeHolderDrawable: Drawable?) {
+        viewBinding.logo.setNftPlaceholder()
+    }
+
+    override fun onBitmapFailed(e: Exception?, errorDrawable: Drawable?) {
+        viewBinding.logo.setNftPlaceholder()
+    }
+
+    override fun onBitmapLoaded(bitmap: Bitmap?, from: Picasso.LoadedFrom?) {
+        viewBinding.logo.setNftBitmap(bitmap)
+    }
+}
+
+fun ImageView.setNftPlaceholder() {
+    scaleType = ImageView.ScaleType.CENTER
+    setImageResource(R.drawable.ic_collectible_placeholder)
+    setBackgroundColor(context.getColorCompat(R.color.whitesmoke_two))
+}
+
+fun ImageView.setNftBitmap(bitmap: Bitmap?) {
+    scaleType = ImageView.ScaleType.FIT_CENTER
+    background = null
+    setImageBitmap(bitmap)
+}
+
+fun ImageView.loadNftImage(logo: String?, target: Target) {
+    when {
+        logo == null -> {
+            setNftPlaceholder()
+        }
+        !logo.isNullOrBlank() -> {
+            Picasso.get()
+                .load(logo)
+                .into(target)
+        }
+        else -> {
+            setNftPlaceholder()
         }
     }
 }
