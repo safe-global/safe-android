3df9280b55a59ef28dcbc1ed015f372a311abb76
Dirk Jäckel <dirk.jaeckel@gnosis.pm>
2020-09-11 08:56:41 +0200

    Fetch tags before trying to read them (#884)

--- ci/prepare_env_buildkite.sh
+++ ci/prepare_env_buildkite.sh
@@ -3,11 +3,9 @@
 set -e
 
 export APP_VERSION_CODE=$((BUILDKITE_BUILD_NUMBER))
+# get newest tags from origin
+git fetch
 # strip the first char as that should always be "v" (as tags should be in the format "vX.X.X")
 description="$(git describe --tags --always)"
 export APP_VERSION_NAME=${description:1}
 export APP_RELEASE_NOTES=$BUILDKITE_MESSAGE
-
-
-
-
