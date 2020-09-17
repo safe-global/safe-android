4d3626b6e6c1d1ca8fbb39401787d520a6d019d5
Vitaly Katz <vitaly@gnosis.pm>
2020-09-17 11:30:52 +0200

    Get version name from gradle for release candidates

--- ci/prepare_env_buildkite.sh
+++ ci/prepare_env_buildkite.sh
@@ -3,9 +3,17 @@
 set -e
 
 export APP_VERSION_CODE=$((BUILDKITE_BUILD_NUMBER))
-# get newest tags from origin
-git fetch
-# strip the first char as that should always be "v" (as tags should be in the format "vX.X.X")
-description="$(git describe --tags --always)"
-export APP_VERSION_NAME=${description:1}
+
+# use version name from gradle for release builds - needs to be updated manually
+if [[ $BUILDKITE_BRANCH  != "release" ]]; then
+  # get newest tags from origin
+  git fetch
+  # strip the first char as that should always be "v" (as tags should be in the format "vX.X.X")
+  description="$(git describe --tags --always)"
+  export APP_VERSION_NAME=${description:1}
+else
+  version="$(./gradlew -q pV | tail -1)"
+  export APP_VERSION_NAME="${version}-${APP_VERSION_CODE}"
+fi
+
 export APP_RELEASE_NOTES=$BUILDKITE_MESSAGE
