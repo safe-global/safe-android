67aeeacfac5ec8545bbe83263860844ab6b7f808
Vitaly Katz <vitaly@gnosis.pm>
2020-10-15 08:32:40 +0200

    Add safe tx hash validation

--- app/src/main/java/io/gnosis/safe/ui/transactions/details/TransactionDetailsViewModel.kt
+++ app/src/main/java/io/gnosis/safe/ui/transactions/details/TransactionDetailsViewModel.kt
@@ -3,20 +3,23 @@ package io.gnosis.safe.ui.transactions.details
 import io.gnosis.data.models.DetailedExecutionInfo
 import io.gnosis.data.models.TransactionDetails
 import io.gnosis.data.models.TransactionStatus
+import io.gnosis.data.repositories.SafeRepository
 import io.gnosis.data.repositories.TransactionRepository
+import io.gnosis.data.utils.calculateSafeTxHash
 import io.gnosis.safe.ui.base.AppDispatchers
 import io.gnosis.safe.ui.base.BaseStateViewModel
 import io.gnosis.safe.utils.OwnerCredentialsRepository
+import pm.gnosis.utils.toHexString
 import javax.inject.Inject
 
 class TransactionDetailsViewModel
 @Inject constructor(
     private val transactionRepository: TransactionRepository,
+    private val safeRepository: SafeRepository,
     private val ownerCredentialsRepository: OwnerCredentialsRepository,
     appDispatchers: AppDispatchers
 ) : BaseStateViewModel<TransactionDetailsViewState>(appDispatchers) {
-
-
+    
     override fun initialState() = TransactionDetailsViewState(ViewAction.Loading(true))
 
     fun loadDetails(txId: String) {
@@ -34,6 +37,20 @@ class TransactionDetailsViewModel
                 true == ownerCredentialsRepository.retrieveCredentials()?.let { credentials ->
             executionInfo.signers.contains(credentials.address) && !executionInfo.confirmations.map { it.signer }.contains(credentials.address)
         }
+
+    suspend fun validateSafeTxHash(transaction: TransactionDetails): Boolean {
+        kotlin.runCatching {
+            val safe = safeRepository.getActiveSafe()
+            val safeTxHash = (transaction.detailedExecutionInfo as DetailedExecutionInfo.MultisigExecutionDetails).safeTxHash
+            val calculatedSafeTxHash = calculateSafeTxHash(safe!!.address, transaction)?.toHexString()
+            safeTxHash == calculatedSafeTxHash
+        }.onSuccess {
+            return it
+        }.onFailure {
+            return false
+        }
+        return false
+    }
 }
 
 data class TransactionDetailsViewState(
