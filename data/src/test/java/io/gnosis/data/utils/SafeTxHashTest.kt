28f6762d1dcf98e65b610e0528eba3662f2301a0
Jose Pablo Alvarez Lacasia <jose.alvarez@gnosis.io>
2020-10-20 11:17:28 +0200

    Refactored some tests and add some others

--- data/src/test/java/io/gnosis/data/utils/SafeTxHashTest.kt
+++ data/src/test/java/io/gnosis/data/utils/SafeTxHashTest.kt
@@ -4,6 +4,7 @@ import io.gnosis.data.adapters.dataMoshi
 import io.gnosis.data.backend.GatewayApi
 import io.gnosis.data.backend.dto.GateTransactionDetailsDto
 import io.gnosis.data.models.DetailedExecutionInfo
+import io.gnosis.data.readJsonFrom
 import io.gnosis.data.repositories.TransactionRepository
 import io.mockk.coEvery
 import io.mockk.mockk
@@ -13,26 +14,20 @@ import org.junit.Test
 import pm.gnosis.utils.addHexPrefix
 import pm.gnosis.utils.asEthereumAddress
 import pm.gnosis.utils.toHexString
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.util.stream.Collectors
 
 class SafeTxHashTest {
 
     private val gatewayApi = mockk<GatewayApi>()
     private val transactionRepository = TransactionRepository(gatewayApi)
 
-    private val moshi = dataMoshi
-    private val txDtoAdapter = moshi.adapter(GateTransactionDetailsDto::class.java)
+    private val txDtoAdapter = dataMoshi.adapter(GateTransactionDetailsDto::class.java)
 
     private val safeAddress = "0x1230B3d59858296A31053C1b8562Ecf89A2f888b".asEthereumAddress()!!
 
     @Test
     fun `calculateSafeTxHash (safe, customTx) should return same value as in customTx`() = runBlocking {
-
-        val json = readResource("tx_details_custom.json")
-        val txCustomDto = txDtoAdapter.fromJson(json)
-        coEvery { gatewayApi.loadTransactionDetails(any()) } returns txCustomDto!!
+        val txCustomDto = txDtoAdapter.readJsonFrom("tx_details_custom.json")
+        coEvery { gatewayApi.loadTransactionDetails(any()) } returns txCustomDto
         val txCustom = transactionRepository.getTransactionDetails("id")
         val executionInfo = txCustom.detailedExecutionInfo as DetailedExecutionInfo.MultisigExecutionDetails
 
@@ -44,10 +39,8 @@ class SafeTxHashTest {
 
     @Test
     fun `calculateSafeTxHash (safe, transferTx) should return same value as in transferTx`() = runBlocking {
-
-        val json = readResource("tx_details_transfer.json")
-        val txTransferDto = txDtoAdapter.fromJson(json)
-        coEvery { gatewayApi.loadTransactionDetails(any()) } returns txTransferDto!!
+        val txTransferDto = txDtoAdapter.readJsonFrom("tx_details_transfer.json")
+        coEvery { gatewayApi.loadTransactionDetails(any()) } returns txTransferDto
         val txTransfer = transactionRepository.getTransactionDetails("id")
         val executionInfo = txTransfer.detailedExecutionInfo as DetailedExecutionInfo.MultisigExecutionDetails
 
@@ -59,10 +52,8 @@ class SafeTxHashTest {
 
     @Test
     fun `calculateSafeTxHash (safe, settingsChangeTx) should return same value as in settingsChangeTx`() = runBlocking {
-
-        val json = readResource("tx_details_settings_change.json")
-        val txSettingsChangeDto = txDtoAdapter.fromJson(json)
-        coEvery { gatewayApi.loadTransactionDetails(any()) } returns txSettingsChangeDto!!
+        val txSettingsChangeDto = txDtoAdapter.readJsonFrom("tx_details_settings_change.json")
+        coEvery { gatewayApi.loadTransactionDetails(any()) } returns txSettingsChangeDto
         val txSettingsChange = transactionRepository.getTransactionDetails("id")
         val executionInfo = txSettingsChange.detailedExecutionInfo as DetailedExecutionInfo.MultisigExecutionDetails
 
@@ -72,11 +63,4 @@ class SafeTxHashTest {
         assertEquals(txCustomSafeTxHash, calculatedTxHash)
     }
 
-    private fun readResource(fileName: String): String {
-        return BufferedReader(
-            InputStreamReader(
-                this::class.java.getClassLoader()?.getResourceAsStream(fileName)!!
-            )
-        ).lines().parallel().collect(Collectors.joining("\n"))
-    }
 }
