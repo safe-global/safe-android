418b319db87ba90c0f6f8c9b898cc417afb177fc
Vitaly Katz <vitaly@gnosis.pm>
2020-10-20 10:23:57 +0200

    Fix tests

--- data/src/test/java/io/gnosis/data/repositories/TransactionRepositoryTest.kt
+++ data/src/test/java/io/gnosis/data/repositories/TransactionRepositoryTest.kt
@@ -259,7 +259,8 @@ private fun buildGateTransactionDto(
         executionInfo = ExecutionInfoDto(
             nonce = 1.toBigInteger(),
             confirmationsSubmitted = 2,
-            confirmationsRequired = 2
+            confirmationsRequired = 2,
+            missingSigners = null
         ),
         timestamp = 1L
     )
