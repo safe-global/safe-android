57445959f64aa9311d0fffe7cf2171b516d3606c
Vitaly Katz <vitaly@gnosis.pm>
2020-11-12 09:40:08 +0100

    Add crash reporting timber tree

--- app/src/main/java/io/gnosis/safe/HeimdallApplication.kt
+++ app/src/main/java/io/gnosis/safe/HeimdallApplication.kt
@@ -1,7 +1,9 @@
 package io.gnosis.safe
 
 import android.content.Context
+import android.util.Log
 import androidx.multidex.MultiDexApplication
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import io.gnosis.safe.di.ComponentProvider
 import io.gnosis.safe.di.components.ApplicationComponent
 import io.gnosis.safe.di.components.DaggerApplicationComponent
@@ -23,9 +25,13 @@ class HeimdallApplication : MultiDexApplication(), ComponentProvider {
 
     override fun onCreate() {
         super.onCreate()
+
         if (BuildConfig.DEBUG) {
             Timber.plant(DebugTree())
+        } else {
+            Timber.plant(CrashReportingTree())
         }
+
         component.appInitManager().init()
 
         try {
@@ -39,6 +45,22 @@ class HeimdallApplication : MultiDexApplication(), ComponentProvider {
     companion object Companion {
         operator fun get(context: Context): ApplicationComponent {
             return (context.applicationContext as ComponentProvider).get()
+        }
+    }
+}
+
+private class CrashReportingTree : Timber.Tree() {
+
+    override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+
+        if (priority == Log.VERBOSE || priority == Log.DEBUG) {
+            return
+        }
+
+        t?.let {
+            if (priority == Log.ERROR) {
+                FirebaseCrashlytics.getInstance().recordException(it)
+            }
         }
     }
 }
