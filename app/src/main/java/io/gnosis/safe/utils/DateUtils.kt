6b8059f51b3207beb3c6c2f83077e47979ab78db
Dirk Jäckel <dirk.jaeckel@gnosis.pm>
2021-01-07 11:31:54 +0100

    Using language "en" and devices country to format dates and times. (#1164)

--- app/src/main/java/io/gnosis/safe/utils/DateUtils.kt
+++ app/src/main/java/io/gnosis/safe/utils/DateUtils.kt
@@ -6,17 +6,17 @@ import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
 import java.util.*
 
-fun Date.formatBackendDateTime(zoneId: ZoneId = ZoneId.systemDefault(), locale: Locale = Locale.getDefault()): String {
+fun Date.formatBackendDateTime(zoneId: ZoneId = ZoneId.systemDefault(), locale: Locale = Locale("en", Locale.getDefault().country)): String {
     val customFormatter: DateTimeFormatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM).withZone(zoneId).withLocale(locale)
     return customFormatter.format(Instant.ofEpochMilli(time))
 }
 
-fun Date.formatBackendDate(zoneId: ZoneId = ZoneId.systemDefault(), locale: Locale = Locale.getDefault()): String {
+fun Date.formatBackendDate(zoneId: ZoneId = ZoneId.systemDefault(), locale: Locale = Locale("en", Locale.getDefault().country)): String {
     val customFormatter: DateTimeFormatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM).withZone(zoneId).withLocale(locale)
     return customFormatter.format(Instant.ofEpochMilli(time))
 }
 
-fun Date.formatBackendTimeOfDay(zoneId: ZoneId = ZoneId.systemDefault(), locale: Locale = Locale.getDefault()): String {
+fun Date.formatBackendTimeOfDay(zoneId: ZoneId = ZoneId.systemDefault(), locale: Locale = Locale("en", Locale.getDefault().country)): String {
     val customFormatter: DateTimeFormatter = DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT).withZone(zoneId).withLocale(locale)
     return customFormatter.format(Instant.ofEpochMilli(time))
 }
