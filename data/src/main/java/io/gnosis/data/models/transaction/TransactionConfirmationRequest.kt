1d545fdb1168d9941385222ae0372ba284a764f4
Vitaly Katz <vitaly@gnosis.pm>
2021-01-08 13:10:28 +0100

    Time formatter for queued & next tx's (#1140)

--- data/src/main/java/io/gnosis/data/models/transaction/TransactionConfirmationRequest.kt
+++ data/src/main/java/io/gnosis/data/models/transaction/TransactionConfirmationRequest.kt
@@ -1,7 +1,9 @@
 package io.gnosis.data.models.transaction
 
 import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 
+@JsonClass(generateAdapter = true)
 data class TransactionConfirmationRequest(
     @Json(name = "signedSafeTxHash")
     val signedSafeTxHash: String
