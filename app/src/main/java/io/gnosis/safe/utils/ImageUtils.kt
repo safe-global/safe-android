e32259dd50777446d4c41c670ddb4622115f4de4
Dirk Jäckel <dirk.jaeckel@gnosis.pm>
2021-01-12 13:37:02 +0100

    Instead of background use two separate images

--- app/src/main/java/io/gnosis/safe/utils/ImageUtils.kt
+++ app/src/main/java/io/gnosis/safe/utils/ImageUtils.kt
@@ -1,18 +1,16 @@
 package io.gnosis.safe.utils
 
-import android.graphics.*
 import android.widget.ImageView
 import androidx.annotation.DrawableRes
 import androidx.core.content.ContextCompat
 import androidx.core.view.setPadding
 import com.squareup.picasso.Picasso
-import com.squareup.picasso.Transformation
 import io.gnosis.safe.R
 
 fun ImageView.loadTokenLogo(
     icon: String?,
     @DrawableRes placeHolderResource: Int = R.drawable.ic_coin_placeholder,
-    @DrawableRes backgroundDrawable: Int? = R.drawable.circle
+    @DrawableRes backgroundDrawable: Int? = null
 ) {
     setPadding(0)
     setImageDrawable(null)
@@ -34,42 +32,10 @@ fun ImageView.loadTokenLogo(
             Picasso.get()
                 .load(icon)
                 .placeholder(placeholderDrawable)
-                .error(placeholderDrawable)
-                .transform(CircleTransformation)
                 .into(this)
         }
         else -> {
             setImageResource(placeHolderResource)
         }
-    }
-}
-
-object CircleTransformation : Transformation {
-    override fun key() = "Circle Transformation"
-
-    override fun transform(source: Bitmap?): Bitmap? {
-        source ?: return null
-        val size = source.width.coerceAtMost(source.height)
-
-        val x = (source.width - size) / 2
-        val y = (source.height - size) / 2
-
-        val squaredBitmap = Bitmap.createBitmap(source, x, y, size, size)
-        if (squaredBitmap != source) {
-            source.recycle()
-        }
-
-        val bitmap = Bitmap.createBitmap(size, size, source.config)
-
-        val canvas = Canvas(bitmap)
-        val paint = Paint()
-        paint.shader = BitmapShader(squaredBitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP)
-        paint.isAntiAlias = true
-
-        val r = size / 2f
-        canvas.drawCircle(r, r, r, paint)
-
-        squaredBitmap.recycle()
-        return bitmap
     }
 }
