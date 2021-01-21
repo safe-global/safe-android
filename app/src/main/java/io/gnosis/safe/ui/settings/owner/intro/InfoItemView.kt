41d239ad8a5d56070d84405808c38ea6a813ea43
Vitaly Katz <vitaly@gnosis.io>
2021-01-21 21:15:25 +0100

    Add InfoItemView

---
+++ app/src/main/java/io/gnosis/safe/ui/settings/owner/intro/InfoItemView.kt
@@ -0,0 +1,45 @@
+package io.gnosis.safe.ui.settings.owner.intro
+
+import android.content.Context
+import android.content.res.TypedArray
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.cardview.widget.CardView
+import io.gnosis.safe.R
+import io.gnosis.safe.databinding.ViewIntroItemBinding
+import timber.log.Timber
+
+class InfoItemView  @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : CardView(context, attrs, defStyleAttr) {
+
+    private val binding = ViewIntroItemBinding.inflate(LayoutInflater.from(context), this, true)
+
+    init {
+        readAttributesAndSetupFields(context, attrs)
+    }
+
+    private fun readAttributesAndSetupFields(context: Context, attrs: AttributeSet?) {
+        context.theme.obtainStyledAttributes(
+            attrs,
+            R.styleable.InfoItem,
+            0, 0
+        ).also {
+            runCatching {
+                applyAttributes(context, it)
+            }
+                .onFailure { Timber.e(it) }
+            it.recycle()
+        }
+    }
+
+    private fun applyAttributes(context: Context, a: TypedArray) {
+        with(binding) {
+            introItemIcon.setImageResource(a.getResourceId(R.styleable.InfoItem_info_icon, -1))
+            introItemTitle.text = a.getString(R.styleable.InfoItem_info_title)
+            introItemText.text = a.getString(R.styleable.InfoItem_info_text)
+        }
+    }
+}
