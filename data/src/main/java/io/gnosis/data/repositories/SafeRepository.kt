9f072ea7642e275a9ea4f4981d80b77d6e76e8c2
Dirk Jäckel <dirk.jaeckel@gnosis.pm>
2021-02-05 17:09:46 +0100

    Revert debug change

--- data/src/main/java/io/gnosis/data/repositories/SafeRepository.kt
+++ data/src/main/java/io/gnosis/data/repositories/SafeRepository.kt
@@ -90,7 +90,7 @@ class SafeRepository(
     }
 
     suspend fun getSafeInfo(safeAddress: Solidity.Address): SafeInfo =
-        transactionServiceApi.getSafeInfo(safeAddress.asEthereumAddressString()).let {
+        transactionServiceApi.getSafeInfo(safeAddress.asEthereumAddressChecksumString()).let {
             SafeInfo(it.address, it.nonce, it.threshold, it.owners, it.masterCopy, it.modules, it.fallbackHandler)
         }
 
