1ba3a594b3efdf94b64fe16ffb74315c0edd3311
Vitaly Katz <vitaly@gnosis.io>
2021-02-19 08:41:08 +0100

    Add rejection confirmation screen; extend nav graph

--- app/src/main/java/io/gnosis/safe/ui/transactions/details/ConfirmRejectionFragment.kt
+++ app/src/main/java/io/gnosis/safe/ui/transactions/details/ConfirmRejectionFragment.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import io.gnosis.safe.R
 import io.gnosis.safe.ScreenId
 import io.gnosis.safe.databinding.FragmentConfirmRejectionBinding
 import io.gnosis.safe.di.components.ViewComponent
 import io.gnosis.safe.ui.base.fragment.BaseViewBindingFragment
+import io.gnosis.safe.utils.appendLink
+import io.gnosis.safe.utils.replaceDoubleNewlineWithParagraphLineSpacing
 
 class ConfirmRejectionFragment : BaseViewBindingFragment<FragmentConfirmRejectionBinding>() {
 
@@ -22,5 +26,18 @@ class ConfirmRejectionFragment : BaseViewBindingFragment<FragmentConfirmRejectio
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        with(binding) {
+            backButton.setOnClickListener {
+                findNavController().navigateUp()
+            }
+            info.text = resources.replaceDoubleNewlineWithParagraphLineSpacing(R.string.tx_details_rejection_confirmation_info)
+            learnMore.appendLink(
+                url = getString(R.string.tx_details_rejection_payment_reason_link),
+                urlText = getString(R.string.tx_details_rejection_payment_reason),
+                linkIcon = R.drawable.ic_external_link_green_16dp,
+                prefix = " ",
+                underline = true
+            )
+        }
     }
 }
---
+++ app/src/main/java/io/gnosis/safe/ui/transactions/details/view/StepIndicator.kt
@@ -0,0 +1,99 @@
+package io.gnosis.safe.ui.transactions.details.view
+
+import android.content.Context
+import android.content.res.TypedArray
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.PointF
+import android.util.AttributeSet
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import io.gnosis.safe.R
+import io.gnosis.safe.databinding.ViewStepIndicatorRejectionStepBinding
+import io.gnosis.safe.utils.dpToPx
+import pm.gnosis.svalinn.common.utils.getColorCompat
+import timber.log.Timber
+
+class StepIndicator @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : LinearLayout(context, attrs, defStyleAttr) {
+
+    private val edgesPaint: Paint
+
+    init {
+        orientation = HORIZONTAL
+        edgesPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+        edgesPaint.style = Paint.Style.FILL_AND_STROKE
+        edgesPaint.strokeWidth = dpToPx(2).toFloat()
+        edgesPaint.color = context.getColorCompat(R.color.text_emphasis_low)
+    }
+
+    override fun dispatchDraw(canvas: Canvas) {
+        super.dispatchDraw(canvas)
+
+        for (i in 0 until childCount - 1) {
+
+            val child1 = getChildAt(i) as Step
+            val child2 = getChildAt(i + 1) as Step
+
+            canvas.drawLine(
+                child1.connectorPointRight.x,
+                child1.connectorPointRight.y,
+                child2.connectorPointLeft.x,
+                child2.connectorPointLeft.y,
+                edgesPaint
+            )
+        }
+    }
+}
+
+interface Step {
+    val connectorPointRight: PointF
+    val connectorPointLeft: PointF
+}
+
+class RejectionStep @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : LinearLayout(context, attrs, defStyleAttr), Step {
+
+    private val binding: ViewStepIndicatorRejectionStepBinding
+
+    init {
+        binding = ViewStepIndicatorRejectionStepBinding.inflate(LayoutInflater.from(context), this)
+        orientation = VERTICAL
+        gravity = Gravity.CENTER
+        readAttributesAndSetupFields(context, attrs)
+    }
+
+    private var connectorMargin: Float = 0f
+
+    override val connectorPointRight: PointF
+        get() = PointF(left + binding.stepIcon.right.toFloat() + connectorMargin, top + binding.stepIcon.height.toFloat() / 2)
+    override val connectorPointLeft: PointF
+        get() = PointF(left + binding.stepIcon.left.toFloat() - connectorMargin, top + binding.stepIcon.height.toFloat() / 2)
+
+    private fun readAttributesAndSetupFields(context: Context, attrs: AttributeSet?) {
+        context.theme.obtainStyledAttributes(
+            attrs,
+            R.styleable.StepIndicator,
+            0, 0
+        ).also {
+            runCatching {
+                applyAttributes(context, it)
+            }
+                .onFailure { Timber.e(it) }
+            it.recycle()
+        }
+    }
+
+    private fun applyAttributes(context: Context, a: TypedArray) {
+        with(binding) {
+            connectorMargin = a.getDimension(R.styleable.StepIndicator_step_connector_margin, 0f)
+            stepIcon.setImageResource(a.getResourceId(R.styleable.StepIndicator_step_icon, -1))
+            stepLabel.text = a.getString(R.styleable.StepIndicator_step_text)
+            val isActiveStep = a.getBoolean(R.styleable.StepIndicator_step_active, true)
+            stepLabel.setTextColor(context.getColorCompat(if (isActiveStep) R.color.text_emphasis_high else R.color.text_emphasis_low))
+        }
+    }
+}
