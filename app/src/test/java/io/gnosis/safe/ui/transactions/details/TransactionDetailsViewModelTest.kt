3d8450282c9afceacf33bbaa048a60b8eb5e5313
Dirk Jäckel <dirk.jaeckel@gnosis.pm>
2021-02-24 17:49:24 +0100

    unit test moved

--- app/src/test/java/io/gnosis/safe/ui/transactions/details/TransactionDetailsViewModelTest.kt
+++ app/src/test/java/io/gnosis/safe/ui/transactions/details/TransactionDetailsViewModelTest.kt
@@ -258,34 +258,6 @@ class TransactionDetailsViewModelTest {
         coVerify(exactly = 1) { tracker.logTransactionConfirmed() }
     }
 
-//    @Test
-//    fun `proposeRejection (successful) emits RejectionSubmitted`() = runBlockingTest {
-//        val transactionDetailsDto = adapter.readJsonFrom("tx_details_transfer.json")
-//        val transactionDetails = toTransactionDetails(transactionDetailsDto)
-//        coEvery { safeRepository.getActiveSafe() } returns Safe("0x1230B3d59858296A31053C1b8562Ecf89A2f888b".asEthereumAddress()!!, "safe_name")
-//        coEvery { safeRepository.getSafes() } returns emptyList()
-//        coEvery { transactionRepository.sign(any(), any()) } returns ""
-//        coEvery { transactionRepository.proposeTransaction(any(), any(), any(), any(), any(), any(), any(), any(), any(), any(), any(), any(), any(), any()) } just Runs
-//        coEvery { ownerCredentialsRepository.retrieveCredentials() } returns OwnerCredentials(
-//            "0x1230B3d59858296A31053C1b8562Ecf89A2f888b".asEthereumAddress()!!,
-//            BigInteger.ONE
-//        )
-//        coEvery { tracker.logTransactionRejected() } just Runs
-//        viewModel.txDetails = transactionDetails
-//
-//        viewModel.submitRejection()
-//
-//        with(viewModel.state.test().values()) {
-//            assertEquals(RejectionSubmitted(viewModel.txDetails!!.toTransactionDetailsViewData(emptyList())), this[0].viewAction)
-//        }
-//        coVerify(exactly = 1) { transactionRepository.proposeTransaction(any(), any(), any(), any(), any(), any(), any(), any(), any(), any(), any(), any(), any(), any()) }
-//        coVerify(exactly = 1) { transactionRepository.sign(BigInteger.ONE, "a64c3d38e98284acabf6c84312dd84817fe58cbf403e7556c5cbb9d57142786a") }
-//        coVerify(exactly = 1) { ownerCredentialsRepository.retrieveCredentials() }
-//        coVerify(exactly = 2) { safeRepository.getActiveSafe() }
-//        coVerify(exactly = 1) { tracker.logTransactionRejected() }
-//    }
-
-
     private suspend fun toTransactionDetails(transactionDetailsDto: TransactionDetails): TransactionDetails {
         val mockGatewayApi = mockk<GatewayApi>().apply { coEvery { loadTransactionDetails(any()) } returns transactionDetailsDto }
         return TransactionRepository(mockGatewayApi).getTransactionDetails("txId")
