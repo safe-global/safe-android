6b3f402d2c129cef35a8ac72700812a9b4a68aef
Dirk Jäckel <dirk.jaeckel@gnosis.pm>
2021-03-17 13:33:03 +0100

    Leave passcode flow on skip

--- app/src/main/java/io/gnosis/safe/ui/settings/app/passcode/CreatePasscodeFragment.kt
+++ app/src/main/java/io/gnosis/safe/ui/settings/app/passcode/CreatePasscodeFragment.kt
@@ -5,10 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import io.gnosis.safe.R
 import io.gnosis.safe.ScreenId
 import io.gnosis.safe.databinding.FragmentSettingsCreatePasscodeBinding
 import io.gnosis.safe.di.components.ViewComponent
+import io.gnosis.safe.ui.base.SafeOverviewBaseFragment.Companion.OWNER_IMPORT_RESULT
 import io.gnosis.safe.ui.base.fragment.BaseViewBindingFragment
+import pm.gnosis.svalinn.common.utils.showKeyboardForView
 
 class CreatePasscodeFragment : BaseViewBindingFragment<FragmentSettingsCreatePasscodeBinding>() {
 
@@ -39,6 +43,12 @@ class CreatePasscodeFragment : BaseViewBindingFragment<FragmentSettingsCreatePas
                 Navigation.findNavController(it).navigateUp()
             }
 
+            input.showKeyboardForView()
+//            input.
+            skipButton.setOnClickListener {
+                findNavController().popBackStack(R.id.createPasscodeFragment, true)
+                findNavController().currentBackStackEntry?.savedStateHandle?.set(OWNER_IMPORT_RESULT, true)
+            }
 //            usePasscode.settingSwitch.isChecked = settingsHandler.usePasscode
 //            usePasscode.settingSwitch.setOnClickListener {
 //                settingsHandler.usePasscode = usePasscode.settingSwitch.isChecked
