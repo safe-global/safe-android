336634c6ef97b837fac39d0626b2f37239871ad0
JohnnyJumper <jeyhunt@gmail.com>
2021-03-29 09:32:17 -0700

    Unstoppabledomains resolution (#1195)

--- app/src/main/java/io/gnosis/safe/di/modules/ViewModule.kt
+++ app/src/main/java/io/gnosis/safe/di/modules/ViewModule.kt
@@ -14,6 +14,7 @@ import io.gnosis.safe.ui.assets.coins.CoinsAdapter
 import io.gnosis.safe.ui.assets.coins.CoinsViewModel
 import io.gnosis.safe.ui.assets.collectibles.CollectiblesViewModel
 import io.gnosis.safe.ui.dialogs.EnsInputViewModel
+import io.gnosis.safe.ui.dialogs.UnstoppableInputViewModel
 import io.gnosis.safe.ui.safe.add.AddSafeNameViewModel
 import io.gnosis.safe.ui.safe.add.AddSafeViewModel
 import io.gnosis.safe.ui.safe.selection.SafeSelectionAdapter
@@ -81,6 +82,10 @@ class ViewModule(
     @Provides
     @ForView
     fun providesEnsInputViewModel(provider: ViewModelProvider) = provider[EnsInputViewModel::class.java]
+
+    @Provides
+    @ForView
+    fun providesUnstoppableInputViewModel(provider: ViewModelProvider) = provider[UnstoppableInputViewModel::class.java]
 
     @Provides
     @ForView
---
+++ data/src/main/java/io/gnosis/data/repositories/UnstoppableDomainsRepository.kt
@@ -0,0 +1,19 @@
+package io.gnosis.data.repositories
+
+import com.unstoppabledomains.resolution.DomainResolution
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import pm.gnosis.model.Solidity
+import pm.gnosis.utils.asEthereumAddress
+
+class UnstoppableDomainsRepository(
+        private val domainResolutionLibrary: DomainResolution
+) {
+
+    suspend fun resolve(domain: String): Solidity.Address {
+        val address = withContext(Dispatchers.IO) {
+            domainResolutionLibrary.getAddress(domain, "eth")
+        };
+        return address.asEthereumAddress()!!;
+    }
+}
