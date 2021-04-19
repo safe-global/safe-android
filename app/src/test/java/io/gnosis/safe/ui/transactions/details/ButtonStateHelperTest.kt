c788477b32f38d92da411ea24e98a8379356355e
Dirk Jäckel <github@dirk.jaeckel.name>
2021-04-19 09:52:39 +0200

    backup

--- app/src/test/java/io/gnosis/safe/ui/transactions/details/ButtonStateHelperTest.kt
+++ app/src/test/java/io/gnosis/safe/ui/transactions/details/ButtonStateHelperTest.kt
@@ -28,24 +28,48 @@ class ButtonStateHelperTest {
     }
 
     @Test
-    fun `(1_2) when (isRejection, awaitingConfirmations, canSign) should hide rejection and enable confirmation button`() {
-//        val buttonStateHelper =
-//            ButtonStateHelper2(
-//                isRejection = true,
-//                hasOwnerKey = true,
-//                hasBeenRejected = true,
-//                awaitingExecution = false,
-//                awaitingConfirmations = false,
-//                canSign = true,
-//            )
-//
-//        assertTrue(buttonStateHelper.confirmationButtonIsVisible())
-//        assertTrue(buttonStateHelper.confirmationButtonIsEnabled())
-//        assertTrue(buttonStateHelper.buttonContainerIsVisible())
-//
-//        assertFalse(buttonStateHelper.rejectionButtonIsVisible())
-//        assertFalse(buttonStateHelper.rejectionButtonIsEnabled())
+    fun `(1_2) when (isRejection && awaitingConfirmations && hasOwnerKey && !canSign) should disable confirmation button and hide rejection button`() {
+
     }
 
+    @Test
+    fun `(1_3) when (All other cases for rejection transaction) should hide both buttons`() {
+
+    }
+
+
+    @Test
+    fun `(2_1) when (!isRejection && awaitingConfirmations && canSign) should enable confirmation `() {
+
+    }
+
+    @Test
+    fun `(2_2) when (!isRejection && awaitingConfirmations && hasOwnerKey && !canSign) should disable confirmation button`() {
+
+    }
+
+    @Test
+    fun `(2_3) when (!isRejection && awaitingConfirmations && hasOwnerKey && !hasBeenRejected) should enable rejection button`() {
+
+    }
+
+    @Test
+    fun `(2_4) when (!isRejection && awaitingConfirmations && hasOwnerKey && hasBeenRejected) should disable rejection button`() {
+
+    }
+    @Test
+    fun `(3_1) when (!isRejection && awaitingExecution && hasOwnerKey && !hasBeenRejected) should enable rejection button full-width`() {
+
+    }
+
+    @Test
+    fun `(3_2) when (!isRejection && awaitingExecution && hasOwnerKey && hasBeenRejected) should hide both buttons`() {
+
+    }
+
+    @Test
+    fun `(3_3) when (awaitingExecution) should hide both buttons `() {
+
+    }
 
 }
