e0f5bfa95db7572e203faf12ec51f6d874456c9d
Vitaly Katz <vitaly@gnosis.pm>
2021-05-10 11:50:12 +0200

    Adjust range separator to '...'  (#1422)

--- app/src/main/java/io/gnosis/safe/utils/SemVer.kt
+++ app/src/main/java/io/gnosis/safe/utils/SemVer.kt
@@ -11,7 +11,7 @@ data class SemVer(
         require(major >= 0) { "Major version must be a positive number" }
         require(minor >= 0) { "Minor version must be a positive number" }
         require(patch >= 0) { "Patch version must be a positive number" }
-        if (preRelease != null) require(preRelease.matches(Regex("""[\dA-z\-]+(?:\.[\dA-z\-]+)*"""))) { "Pre-release version is not valid" }
+        if (preRelease != null) require(preRelease.matches(Regex("""(0|[1-9]\d*(?:rc)?-)?([A-z\_]+)*"""))) { "Pre-release version is not valid" }
     }
 
     fun isInside(rangesList: String): Boolean {
@@ -61,23 +61,27 @@ data class SemVer(
 
     companion object {
 
+        private const val REGEX_PRE_RELEASE = """(0|[1-9]\d*(?:rc)?-)?([A-z\_]+)*"""
+        private const val REGEX_SEM_VER = "(0|[1-9]\\d*)?(?:\\.)?(0|[1-9]\\d*)?(?:\\.)?(0|[1-9]\\d*)?(?:-$REGEX_PRE_RELEASE)?"
+        private const val REGEX_SEM_VER_RANGE = "(${REGEX_SEM_VER})(?:\\.\\.\\.)?(?:(${REGEX_SEM_VER}))?"
+
         fun parse(version: String): SemVer {
-            val pattern = Regex("""(0|[1-9]\d*)?(?:\.)?(0|[1-9]\d*)?(?:\.)?(0|[1-9]\d*)?(?:-([\dA-z\-]+(?:\.[\dA-z\-]+)*))?""")
+            val pattern = Regex(REGEX_SEM_VER)
             val result = pattern.matchEntire(version) ?: throw IllegalArgumentException("Invalid version string [$version]")
             return SemVer(
                 major = if (result.groupValues[1].isEmpty()) 0 else result.groupValues[1].toInt(),
                 minor = if (result.groupValues[2].isEmpty()) 0 else result.groupValues[2].toInt(),
                 patch = if (result.groupValues[3].isEmpty()) 0 else result.groupValues[3].toInt(),
-                preRelease = if (result.groupValues[4].isEmpty()) null else result.groupValues[4]
+                preRelease = if (result.groupValues[5].isEmpty()) null else "${result.groupValues[4]}${result.groupValues[5]}"
             )
         }
 
         fun parseRange(range: String): Pair<SemVer, SemVer?> {
-            val pattern = Regex("""([\dA-z\.]+)(?:-)?([\dA-z\.]+)?""")
+            val pattern = Regex(REGEX_SEM_VER_RANGE)
             val result = pattern.matchEntire(range) ?: throw IllegalArgumentException("Invalid range version string [$range]")
             return when {
-                result.groupValues[2].isEmpty() -> Pair(SemVer.parse(result.groupValues[1]), null)
-                else -> Pair(SemVer.parse(result.groupValues[1]), SemVer.parse(result.groupValues[2]))
+                result.groupValues[7].isEmpty() -> Pair(SemVer.parse(result.groupValues[1]), null)
+                else -> Pair(parse(result.groupValues[1]), SemVer.parse(result.groupValues[7]))
             }
         }
     }
