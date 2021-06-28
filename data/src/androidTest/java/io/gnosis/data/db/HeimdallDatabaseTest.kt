de42976e2dc1057ce060e85d92d03dc6452999b1
Dirk Jäckel <github@dirk.jaeckel.name>
2021-06-28 10:48:05 +0200

    One more test

--- data/src/androidTest/java/io/gnosis/data/db/HeimdallDatabaseTest.kt
+++ data/src/androidTest/java/io/gnosis/data/db/HeimdallDatabaseTest.kt
@@ -10,6 +10,7 @@ import androidx.test.platform.app.InstrumentationRegistry
 import io.gnosis.data.models.Owner
 import io.gnosis.data.models.OwnerTypeConverter
 import io.gnosis.data.models.Safe
+import io.gnosis.data.test.BuildConfig
 import org.junit.Assert
 import org.junit.Assert.assertTrue
 import org.junit.Rule
@@ -153,41 +154,40 @@ class HeimdallDatabaseTest {
             close()
         }
     }
-//
-//    @Test
-//    @Throws(IOException::class)
-//    fun migrate4To5() {
-//
-//        val safe = Safe(Solidity.Address(BigInteger.ONE), "Fnord", BuildConfig.CHAIN_ID)
-//
-//        helper.createDatabase(TEST_DB, 4).apply {
-//
-//            val rowId = insert(
-//                Safe.TABLE_NAME, OnConflictStrategy.REPLACE,
-//                ContentValues().apply {
-//                    put(Safe.COL_ADDRESS, addressConverter.toHexString(safe.address))
-//                    put(Safe.COL_LOCAL_NAME, safe.localName)
-////                    put(Safe.COL_CHAIN_ID, safe.chainId)
-//                })
-//
-//            assertTrue(rowId >= 0)
-//
-//            close()
-//        }
-//
-//        helper.runMigrationsAndValidate(TEST_DB, 5, true, HeimdallDatabase.MIGRATION_4_5).apply {
-//
-//            with(query("SELECT * FROM ${Safe.TABLE_NAME}")) {
-//                Assert.assertEquals(1, count)
-//                moveToFirst()
-//                Assert.assertEquals(getString(getColumnIndex(Safe.COL_ADDRESS)), safe.address.asEthereumAddressString())
-//                Assert.assertEquals(getString(getColumnIndex(Safe.COL_LOCAL_NAME)), safe.localName)
-//                Assert.assertEquals(getInt(getColumnIndex(Safe.COL_CHAIN_ID)), safe.chainId)
-//            }
-//
-//            close()
-//        }
-//    }
+
+    @Test
+    @Throws(IOException::class)
+    fun migrate4To5() {
+
+        val safe = Safe(Solidity.Address(BigInteger.ONE), "Fnord", BuildConfig.CHAIN_ID)
+
+        helper.createDatabase(TEST_DB, 4).apply {
+
+            val rowId = insert(
+                Safe.TABLE_NAME, OnConflictStrategy.REPLACE,
+                ContentValues().apply {
+                    put(Safe.COL_ADDRESS, addressConverter.toHexString(safe.address))
+                    put(Safe.COL_LOCAL_NAME, safe.localName)
+                })
+
+            assertTrue(rowId >= 0)
+
+            close()
+        }
+
+        helper.runMigrationsAndValidate(TEST_DB, 5, true, HeimdallDatabase.MIGRATION_4_5).apply {
+
+            with(query("SELECT * FROM ${Safe.TABLE_NAME}")) {
+                Assert.assertEquals(1, count)
+                moveToFirst()
+                Assert.assertEquals(getString(getColumnIndex(Safe.COL_ADDRESS)), safe.address.asEthereumAddressString())
+                Assert.assertEquals(getString(getColumnIndex(Safe.COL_LOCAL_NAME)), safe.localName)
+                Assert.assertEquals(getInt(getColumnIndex(Safe.COL_CHAIN_ID)), safe.chainId)
+            }
+
+            close()
+        }
+    }
 
     companion object {
         private const val TEST_DB = "migration-test"
