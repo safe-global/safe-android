69bc105516750066ad73ac048bcb0baf79fd2553
Vitaly Katz <vitaly@gnosis.io>
2021-07-02 11:48:28 +0200

    Fix test

--- app/src/test/java/io/gnosis/safe/utils/StringUtilsKtTest.kt
+++ app/src/test/java/io/gnosis/safe/utils/StringUtilsKtTest.kt
@@ -1,10 +1,13 @@
 package io.gnosis.safe.utils
 
 import android.content.Context
+import android.graphics.Color
 import io.gnosis.safe.R
 import io.mockk.every
 import io.mockk.mockk
-import junit.framework.Assert.assertEquals
+import io.mockk.mockkStatic
+import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Test
 import pm.gnosis.svalinn.common.utils.getColorCompat
 
@@ -12,23 +15,25 @@ class StringUtilsKtTest {
 
     private val context = mockk<Context>(relaxed = true)
 
+    @Before
+    fun setup() {
+        mockkStatic(Color::class)
+    }
 
     @Test
     fun `toColor with #RRGGBB should return right color`() {
         every { context.getColorCompat(any()) } returns 23
-//        mockkStatic(Context::class)
-//        mockkStatic(Color::class)
+        every { Color.parseColor(any()) } returns Color.BLACK
 
-        val result = "#112233".toColor(context)
+        val result = "#ffffff".toColor(context)
 
-        assertEquals(-12345, result)
+        assertEquals(Color.BLACK, result)
     }
 
     @Test
     fun `toColor with unsupported format should return failsafe color`() {
         every { context.getColorCompat(any()) } returns 23
-//        mockkStatic(Context::class)
-//        mockkStatic(Color::class)
+        every { Color.parseColor(any()) } throws Exception()
 
         val result = "#123".toColor(context, R.color.white)
 
