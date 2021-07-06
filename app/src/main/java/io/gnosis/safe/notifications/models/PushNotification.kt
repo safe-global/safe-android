d2acab44c9d8cc101d791c878172958f339e07b2
Dirk Jäckel <github@dirk.jaeckel.name>
2021-07-06 09:16:00 +0200

    chainId in base class

--- app/src/main/java/io/gnosis/safe/notifications/models/PushNotification.kt
+++ app/src/main/java/io/gnosis/safe/notifications/models/PushNotification.kt
@@ -7,79 +7,79 @@ import java.math.BigInteger
 sealed class PushNotification(
     val type: String
 ) {
-
+    abstract val chainId: Int
     abstract val safe: Solidity.Address
 
     data class ConfirmationRequest(
+        override val chainId: Int,
         override val safe: Solidity.Address,
-        val safeTxHash: String,
-        val chainId: Int
+        val safeTxHash: String
     ) : PushNotification(TYPE) {
         companion object {
             const val TYPE = "CONFIRMATION_REQUEST"
             fun fromMap(params: Map<String, String>) =
                 ConfirmationRequest(
+                    params.getOrThrow("chainId").toInt(),
                     params.getOrThrow("address").asEthereumAddress()!!,
-                    params.getOrThrow("safeTxHash"),
-                    params.getOrThrow("chainId").toInt()
+                    params.getOrThrow("safeTxHash")
                 )
         }
     }
 
     data class ExecutedTransaction(
+        override val chainId: Int,
         override val safe: Solidity.Address,
         val safeTxHash: String,
-        val failed: Boolean,
-        val chainId: Int
+        val failed: Boolean
     ) : PushNotification(TYPE) {
         companion object {
             const val TYPE = "EXECUTED_MULTISIG_TRANSACTION"
             fun fromMap(params: Map<String, String>) =
                 ExecutedTransaction(
+                    params.getOrThrow("chainId").toInt(),
                     params.getOrThrow("address").asEthereumAddress()!!,
                     params.getOrThrow("safeTxHash"),
-                    params.getOrThrow("failed").toBoolean(),
-                    params.getOrThrow("chainId").toInt()
+                    params.getOrThrow("failed").toBoolean()
                 )
         }
     }
 
     data class IncomingToken(
+        override val chainId: Int,
         override val safe: Solidity.Address,
         val txHash: String,
-        val tokenAddress: Solidity.Address,
-        val value: BigInteger? = null, // null for ERC721 tokens
-        val tokenId: String? = null,    // null for ERC20 tokens
-        val chainId: Int
+        val tokenAddress: Solidity.Address, // null for ERC721 tokens
+        val value: BigInteger? = null,    // null for ERC20 tokens
+        val tokenId: String? = null
     ) : PushNotification(TYPE) {
         companion object {
             const val TYPE = "INCOMING_TOKEN"
             fun fromMap(params: Map<String, String>) =
                 IncomingToken(
+                    params.getOrThrow("chainId").toInt(),
                     params.getOrThrow("address").asEthereumAddress()!!,
                     params.getOrThrow("txHash"),
                     params.getOrThrow("tokenAddress").asEthereumAddress()!!,
                     params["value"]?.toBigInteger(),
-                    params["tokenId"],
-                    params.getOrThrow("chainId").toInt()
+                    params["tokenId"]
                 )
         }
     }
 
     data class IncomingEther(
+        override val chainId: Int,
         override val safe: Solidity.Address,
         val txHash: String,
-        val value: BigInteger,
-        val chainId: Int
+        val value: BigInteger
     ) : PushNotification(TYPE) {
         companion object {
             const val TYPE = "INCOMING_ETHER"
             fun fromMap(params: Map<String, String>) =
                 IncomingEther(
+                    params.getOrThrow("chainId").toInt(),
                     params.getOrThrow("address").asEthereumAddress()!!,
                     params.getOrThrow("txHash"),
-                    params.getOrThrow("value").toBigInteger(),
-                    params.getOrThrow("chainId").toInt()
+                    params.getOrThrow("value").toBigInteger()
                 )
         }
     }
