14bfe3e7d8db3ff419d072c244c6d3e920e6c16f
Dirk Jäckel <dirk.jaeckel@gnosis.pm>
2021-07-07 10:32:21 +0200

    Move UD resolver creation to repository (#1559)
    * Move UD resolver creation to repository
    * Disable ud option on incompatible chains
    * Increase test covcerage

--- app/src/main/java/io/gnosis/safe/ui/dialogs/UnstoppableInputViewModel.kt
+++ app/src/main/java/io/gnosis/safe/ui/dialogs/UnstoppableInputViewModel.kt
@@ -1,25 +1,26 @@
 package io.gnosis.safe.ui.dialogs
 
 import androidx.lifecycle.ViewModel
+import io.gnosis.data.models.Chain
 import io.gnosis.data.repositories.UnstoppableDomainsRepository
 import pm.gnosis.model.Solidity
 import javax.inject.Inject
 
 class UnstoppableInputViewModel
 @Inject constructor(
-        private val unstoppableRepository: UnstoppableDomainsRepository
+    private val unstoppableRepository: UnstoppableDomainsRepository
 ) : ViewModel() {
 
-    suspend fun processInput(input: CharSequence): Solidity.Address {
+    suspend fun processInput(input: CharSequence, chain: Chain): Solidity.Address {
         return kotlin.runCatching {
-            unstoppableRepository.resolve(input.toString())
+            unstoppableRepository.resolve(input.toString(), chain.chainId)
         }
-                .onSuccess {
-                    it
-                }
-                .onFailure {
-                   throw it
-                }
-                .getOrNull()!!
+            .onSuccess {
+                it
+            }
+            .onFailure {
+                throw it
+            }
+            .getOrNull()!!
     }
 }
