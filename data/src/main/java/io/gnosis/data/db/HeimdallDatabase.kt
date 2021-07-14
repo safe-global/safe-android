e8eb305609d5d861f3ba25d01aca2ac8dcac3f75
Vitaly Katz <vitaly@gnosis.io>
2021-07-14 07:40:19 +0200

    Fix db migration

--- data/src/main/java/io/gnosis/data/db/HeimdallDatabase.kt
+++ data/src/main/java/io/gnosis/data/db/HeimdallDatabase.kt
@@ -67,6 +67,7 @@ abstract class HeimdallDatabase : RoomDatabase() {
 
         val MIGRATION_4_5 = object : Migration(4, 5) {
             override fun migrate(database: SupportSQLiteDatabase) {
+                val defaultChainId = BigIntegerConverter().toHexString(BuildConfig.CHAIN_ID.toBigInteger())
                 database.execSQL(
                     """CREATE TABLE IF NOT EXISTS `${Chain.TABLE_NAME}` (`${Chain.COL_CHAIN_NAME}` TEXT NOT NULL, `${Chain.COL_TEXT_COLOR}` TEXT NOT NULL, `${Chain.COL_BACKGROUND_COLOR}` TEXT NOT NULL, `${Chain.COL_CHAIN_ID}` TEXT NOT NULL, `${Chain.COL_ENS_REGISTRY_ADDRESS}` TEXT, PRIMARY KEY(`${Chain.COL_CHAIN_ID}`))"""
                 )
@@ -76,13 +77,13 @@ abstract class HeimdallDatabase : RoomDatabase() {
 
                 // Add CHAIN_ID column and add it to primary key
                 database.execSQL(
-                    """ALTER TABLE `${SafeMetaData.TABLE_NAME}` ADD COLUMN `${SafeMetaData.COL_CHAIN_ID}` TEXT NOT NULL DEFAULT `${BuildConfig.CHAIN_ID}`"""
+                    """ALTER TABLE `${SafeMetaData.TABLE_NAME}` ADD COLUMN `${SafeMetaData.COL_CHAIN_ID}` TEXT NOT NULL DEFAULT `${defaultChainId}`"""
                 )
                 database.execSQL(
                     """ALTER TABLE `${SafeMetaData.TABLE_NAME}` RENAME TO `${SafeMetaData.TABLE_NAME}_old`"""
                 )
                 database.execSQL(
-                    """CREATE TABLE `${SafeMetaData.TABLE_NAME}` (`address` TEXT NOT NULL, `chain_id` TEXT NOT NULL, `registered_notifications` INTEGER NOT NULL, PRIMARY KEY(`address`, `chain_id`), FOREIGN KEY(`address`, `chain_id`) REFERENCES `safes`(`address`, `chain_id`) ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED)"""
+                    """CREATE TABLE `${SafeMetaData.TABLE_NAME}` (`address` TEXT NOT NULL, `chain_id` TEXT NOT NULL DEFAULT `${defaultChainId}`, `registered_notifications` INTEGER NOT NULL, PRIMARY KEY(`address`, `chain_id`), FOREIGN KEY(`address`, `chain_id`) REFERENCES `safes`(`address`, `chain_id`) ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED)"""
                 )
                 database.execSQL(
                     """INSERT INTO ${SafeMetaData.TABLE_NAME} SELECT * FROM ${SafeMetaData.TABLE_NAME}_old"""
@@ -93,13 +94,13 @@ abstract class HeimdallDatabase : RoomDatabase() {
 
                 // Add CHAIN_ID column and add it to primary key
                 database.execSQL(
-                    """ALTER TABLE `${Safe.TABLE_NAME}` ADD COLUMN `${Safe.COL_CHAIN_ID}` TEXT NOT NULL DEFAULT `${BuildConfig.CHAIN_ID}`"""
+                    """ALTER TABLE `${Safe.TABLE_NAME}` ADD COLUMN `${Safe.COL_CHAIN_ID}` TEXT NOT NULL DEFAULT `${defaultChainId}`"""
                 )
                 database.execSQL(
                     """ALTER TABLE `${Safe.TABLE_NAME}` RENAME TO `${Safe.TABLE_NAME}_old`"""
                 )
                 database.execSQL(
-                    """CREATE TABLE `${Safe.TABLE_NAME}` (`address` TEXT NOT NULL, `local_name` TEXT NOT NULL, `chain_id` TEXT NOT NULL, PRIMARY KEY(`address`, `chain_id`))"""
+                    """CREATE TABLE `${Safe.TABLE_NAME}` (`address` TEXT NOT NULL, `local_name` TEXT NOT NULL, `chain_id` TEXT NOT NULL DEFAULT `${defaultChainId}`, PRIMARY KEY(`address`, `chain_id`))"""
                 )
                 database.execSQL(
                     """INSERT INTO ${Safe.TABLE_NAME} SELECT * FROM ${Safe.TABLE_NAME}_old"""
