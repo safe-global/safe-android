63922cc99d979269c4c1524eb019040db28f992e
Vitaly Katz <vitaly@gnosis.io>
2021-07-16 06:56:35 +0200

    Adjust registration endpoint & models

--- app/src/main/java/io/gnosis/safe/notifications/NotificationServiceApi.kt
+++ app/src/main/java/io/gnosis/safe/notifications/NotificationServiceApi.kt
@@ -6,20 +6,17 @@ import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.POST
 import retrofit2.http.Path
+import java.math.BigInteger
 
 interface NotificationServiceApi {
 
-    @POST("/api/v1/notifications/devices/")
-    suspend fun register(@Body data: Registration): Registration
+    @POST("/v1/register/notifications")
+    suspend fun register(@Body data: Registration)
 
-    @DELETE("/api/v1/notifications/devices/{uuid}")
-    suspend fun unregister(@Path("uuid") deviceUuid: String)
-
-    @DELETE("/api/v1/notifications/devices/{uuid}/safes/{address}/")
-    suspend fun unregisterSafe(@Path("uuid") deviceUuid: String, @Path("address") safe: String)
+    @DELETE("/v1/chains/{chainId}/notifications/devices/{uuid}/safes/{address}")
+    suspend fun unregisterSafe(@Path("uuid") deviceUuid: String, @Path("chainId") chainId: BigInteger, @Path("address") safe: String)
 
     companion object {
-        //const val BASE_URL = "https://safe-transaction.dev.gnosisdev.com/"
-        const val BASE_URL = BuildConfig.TRANSACTION_SERVICE_URL
+        const val BASE_URL = BuildConfig.CLIENT_GATEWAY_URL
     }
 }
