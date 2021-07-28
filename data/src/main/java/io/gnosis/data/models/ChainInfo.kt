f7e29470e48d40aefe6a1008f72cb85d7dfdd5fc
Vitaly Katz <vitaly@gnosis.io>
2021-07-28 11:10:18 +0200

    Fix

--- data/src/main/java/io/gnosis/data/models/ChainInfo.kt
+++ data/src/main/java/io/gnosis/data/models/ChainInfo.kt
@@ -12,7 +12,7 @@ data class ChainInfo(
     @Json(name = "chainName") val chainName: String,
     @Json(name = "ensRegistryAddress") val ensRegistryAddress: String?,
     @Json(name = "rpcUri") val rpcUri: RpcUri,
-    @Json(name = "blockExplorerUri") val blockExplorerUrl: String,
+    @Json(name = "blockExplorerUriTemplate") val blockExplorerUrl: String,
     @Json(name = "nativeCurrency") val nativeCurrency: NativeCurrency,
     @Json(name = "transactionService") val transactionService: String,
     @Json(name = "theme") val theme: ChainTheme
