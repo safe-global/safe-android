184fd029c016c90c16d8f5586b488d02becaefb3
Dirk Jäckel <github@dirk.jaeckel.name>
2021-08-21 13:33:54 +0200

    Added tests for formatEthAddress

--- app/src/test/java/io/gnosis/safe/utils/StringUtilsKtTest.kt
+++ app/src/test/java/io/gnosis/safe/utils/StringUtilsKtTest.kt
@@ -2,6 +2,12 @@ package io.gnosis.safe.utils
 
 import android.content.Context
 import android.graphics.Color
+import android.graphics.Typeface
+import android.text.style.ForegroundColorSpan
+import android.text.style.StyleSpan
+import androidx.core.text.getSpans
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import io.gnosis.safe.R
 import io.mockk.every
 import io.mockk.mockk
@@ -9,11 +15,15 @@ import io.mockk.mockkStatic
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
 import pm.gnosis.svalinn.common.utils.getColorCompat
+import pm.gnosis.utils.asEthereumAddress
 
+@RunWith(AndroidJUnit4::class)
 class StringUtilsKtTest {
 
     private val context = mockk<Context>(relaxed = true)
+    private val applicationContext: Context = ApplicationProvider.getApplicationContext()
 
     @Before
     fun setup() {
@@ -38,5 +48,113 @@ class StringUtilsKtTest {
         val result = "#123".toColor(context, R.color.white)
 
         assertEquals(23, result)
+    }
+
+    @Test
+    fun `setLink`() {
+    }
+
+    @Test
+    fun `appendLink`() {
+    }
+
+    @Test
+    fun `replaceDoubleNewlineWithParagraphLineSpacing`() {
+    }
+
+    @Test
+    fun `appendTextWithSpans`() {
+    }
+
+    @Test
+    fun `formatForTxList`() {
+    }
+
+    @Test
+    fun `abbreviateEthAddress`() {
+    }
+
+    @Test
+    fun `formatEthAddressBold`() {
+    }
+
+    @Test
+    fun `fallBackHandlerLabel`() {
+    }
+    
+    @Test
+    fun `formatEthAddress (with middleLineBreak)`() {
+        val address = "0xd5D82B6aDDc9027B22dCA772Aa68D5d74cdBdF44".asEthereumAddress()!!
+
+        val spannable = address.formatEthAddress(applicationContext)
+
+        assertEquals('\n', spannable.get(21)) // check linebreak
+        assertEquals(43, spannable.length)
+    }
+
+    @Test
+    fun `formatEthAddress (no middleLineBreak)`() {
+        val address = "0xd5D82B6aDDc9027B22dCA772Aa68D5d74cdBdF44".asEthereumAddress()!!
+
+        val spannable = address.formatEthAddress(context = applicationContext, addMiddleLinebreak = false)
+
+        assertEquals('C', spannable.get(21)) // check no linebreak
+        assertEquals(42, spannable.length)
+    }
+
+    @Test
+    fun `formatEthAddress (not checksummed)`() {
+        val address = "0xd5d82B6addc9027b22dca772Aa68d5d74cdbdf44".asEthereumAddress()!!
+
+        val spannable = address.formatEthAddress(context = applicationContext, addMiddleLinebreak = false)
+
+        assertEquals("0xd5D82B6aDDc9027B22dCA772Aa68D5d74cdBdF44", spannable.toString())
+
+        val prefixLength = 6
+        val suffixLength = 4
+
+        val foregroundColorSpans = spannable.getSpans<ForegroundColorSpan>(start = 0, end = prefixLength)
+        assertEquals(foregroundColorSpans.size, 1)
+        assertEquals(applicationContext.getColorCompat(R.color.address_boundaries), foregroundColorSpans[0].foregroundColor)
+
+        val styleSpans = spannable.getSpans<StyleSpan>(start = 0, end = prefixLength)
+        assertEquals(styleSpans.size, 1)
+        assertEquals(Typeface.BOLD, styleSpans[0].style)
+
+        val foregroundColorSpansEnd = spannable.getSpans<ForegroundColorSpan>(start = spannable.length - suffixLength, end = spannable.length)
+        assertEquals(foregroundColorSpansEnd.size, 1)
+        assertEquals(applicationContext.getColorCompat(R.color.address_boundaries), foregroundColorSpansEnd[0].foregroundColor)
+
+        val styleSpansEnd = spannable.getSpans<StyleSpan>(start = spannable.length - suffixLength, end = spannable.length)
+        assertEquals(styleSpansEnd.size, 1)
+        assertEquals(Typeface.BOLD, styleSpansEnd[0].style)
+
+        val foregroundColorSpansMiddle = spannable.getSpans<ForegroundColorSpan>(start = prefixLength, end = spannable.length - suffixLength)
+        assertEquals(foregroundColorSpansMiddle.size, 1)
+        assertEquals(applicationContext.getColorCompat(R.color.text_emphasis_low), foregroundColorSpansMiddle[0].foregroundColor)
+
+        val spans = spannable.getSpans<Any>()
+        assertEquals(5, spans.size)
+
+    }
+
+    @Test
+    fun `shortChecksumString`() {
+    }
+
+    @Test
+    fun `parseEthereumAddress`() {
+    }
+
+    @Test
+    fun `underline`() {
+    }
+
+    @Test
+    fun `asMiddleEllipsized(boundariesLength)`() {
+    }
+
+    @Test
+    fun `asMiddleEllipsized(prefixLength, suffixLength)`() {
     }
 }
