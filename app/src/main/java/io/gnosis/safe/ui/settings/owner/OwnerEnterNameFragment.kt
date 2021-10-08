15394261a28efcca7ce816e95d7d7667cf3878d2
Vitaly Katz <vitaly@gnosis.io>
2021-10-08 15:21:03 +0200

    Fix color of owner type icon

--- app/src/main/java/io/gnosis/safe/ui/settings/owner/OwnerEnterNameFragment.kt
+++ app/src/main/java/io/gnosis/safe/ui/settings/owner/OwnerEnterNameFragment.kt
@@ -20,7 +20,7 @@ import io.gnosis.safe.ui.base.BaseStateViewModel.ViewAction.NavigateTo
 import io.gnosis.safe.ui.base.SafeOverviewBaseFragment
 import io.gnosis.safe.ui.base.fragment.BaseViewBindingFragment
 import io.gnosis.safe.ui.settings.owner.ledger.LedgerController.Companion.LEDGER_PATH
-import io.gnosis.safe.ui.settings.owner.list.imageRes24dp
+import io.gnosis.safe.ui.settings.owner.list.imageRes24dpWhite
 import io.gnosis.safe.utils.formatEthAddress
 import pm.gnosis.svalinn.common.utils.hideSoftKeyboard
 import pm.gnosis.utils.asEthereumAddress
@@ -61,7 +61,7 @@ class OwnerEnterNameFragment : BaseViewBindingFragment<FragmentOwnerNameEnterBin
         with(binding) {
             newAddressBlockies.setAddress(ownerAddress)
             newAddressHex.text = ownerAddress.formatEthAddress(requireContext(), addMiddleLinebreak = false)
-            keyType.setImageResource(ownerType.imageRes24dp())
+            keyType.setImageResource(ownerType.imageRes24dpWhite())
             backButton.setOnClickListener { findNavController().navigateUp() }
 
             when (ownerType) {
