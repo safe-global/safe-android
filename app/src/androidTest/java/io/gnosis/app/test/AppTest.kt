31cef2aaf7ea144058fe097bc7460e391fff7de9
Dirk Jäckel <github@dirk.jaeckel.name>
2022-08-17 21:28:59 +0200

    Have a dummy test in app so the build doesn't break :-(

---
+++ app/src/androidTest/java/io/gnosis/app/test/AppTest.kt
@@ -0,0 +1,14 @@
+package io.gnosis.app.test
+
+import androidx.test.runner.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+
+// https://developer.android.com/training/data-storage/room/migrating-db-versions
+@RunWith(AndroidJUnit4::class)
+class AppTest {
+
+    @Test
+    fun dummy() {
+    }
+}
