b51cf4e87f1a7f80b37247f6b636e6d9636cc085
Vitaly Katz <vitaly@gnosis.io>
2022-10-13 12:18:32 +0200

    Show address input dialog

--- app/src/main/java/io/gnosis/safe/ui/safe/send_funds/SendAssetFragment.kt
+++ app/src/main/java/io/gnosis/safe/ui/safe/send_funds/SendAssetFragment.kt
@@ -10,8 +10,10 @@ import io.gnosis.safe.R
 import io.gnosis.safe.ScreenId
 import io.gnosis.safe.databinding.FragmentSendAssetBinding
 import io.gnosis.safe.di.components.ViewComponent
+import io.gnosis.safe.helpers.AddressInputHelper
 import io.gnosis.safe.ui.base.fragment.BaseViewBindingFragment
 import io.gnosis.safe.utils.toColor
+import pm.gnosis.model.Solidity
 import javax.inject.Inject
 
 class SendAssetFragment : BaseViewBindingFragment<FragmentSendAssetBinding>() {
@@ -25,6 +27,18 @@ class SendAssetFragment : BaseViewBindingFragment<FragmentSendAssetBinding>() {
 
     @Inject
     lateinit var viewModel: SendAssetViewModel
+
+    private val addressInputHelper by lazy {
+        AddressInputHelper(
+            fragment = this,
+            tracker = tracker,
+            selectedChain = selectedChain,
+            addressCallback = ::updateAddress,
+            errorCallback = ::handleError,
+            enableUD = viewModel.enableUD(selectedChain),
+            enableENS = viewModel.enableENS(selectedChain)
+        )
+    }
 
     override fun inject(component: ViewComponent) {
         component.inject(this)
@@ -55,6 +69,21 @@ class SendAssetFragment : BaseViewBindingFragment<FragmentSendAssetBinding>() {
                     R.color.primary
                 )
             )
+            recepientAddressInputLayout.setOnClickListener {
+                addressInputHelper.showDialog()
+            }
         }
+    }
+
+    private fun updateAddress(address: Solidity.Address) {
+
+    }
+
+    private fun handleValid(address: Solidity.Address) {
+
+    }
+
+    private fun handleError(throwable: Throwable, input: String? = null) {
+
     }
 }
--- app/src/main/java/io/gnosis/safe/ui/safe/send_funds/SendAssetViewModel.kt
+++ app/src/main/java/io/gnosis/safe/ui/safe/send_funds/SendAssetViewModel.kt
@@ -1,17 +1,29 @@
 package io.gnosis.safe.ui.safe.send_funds
 
+import io.gnosis.data.models.Chain
+import io.gnosis.data.repositories.EnsRepository
+import io.gnosis.data.repositories.UnstoppableDomainsRepository
 import io.gnosis.safe.ui.base.AppDispatchers
 import io.gnosis.safe.ui.base.BaseStateViewModel
 import javax.inject.Inject
 
 class SendAssetViewModel
 @Inject constructor(
+    private val unstoppableDomainsRepository: UnstoppableDomainsRepository,
+    private val ensRepository: EnsRepository,
     appDispatchers: AppDispatchers
 ) : BaseStateViewModel<SendAssetState>(appDispatchers) {
 
     override fun initialState(): SendAssetState =
         SendAssetState(viewAction = null)
 
+    fun enableUD(chain: Chain): Boolean {
+        return unstoppableDomainsRepository.canResolve(chain)
+    }
+
+    fun enableENS(chain: Chain): Boolean {
+        return ensRepository.canResolve(chain)
+    }
 }
 
 data class SendAssetState(
