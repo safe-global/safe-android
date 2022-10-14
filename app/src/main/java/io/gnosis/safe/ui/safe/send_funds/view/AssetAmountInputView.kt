6591899c16560ce46df1fcf773b46fe61c257c52
Vitaly Katz <vitaly@gnosis.io>
2022-10-14 12:12:37 +0200

    Custom view for amount input

---
+++ app/src/main/java/io/gnosis/safe/ui/safe/send_funds/view/AssetAmountInputView.kt
@@ -0,0 +1,13 @@
+package io.gnosis.safe.ui.safe.send_funds.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.LinearLayout
+
+class AssetAmountInputView@JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : LinearLayout(context, attrs, defStyleAttr) {
+
+}
