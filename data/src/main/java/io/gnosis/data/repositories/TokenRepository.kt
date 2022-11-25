50a6d3a77d4f429efb2987b2d6eecf98deb81c7f
Vitaly Katz <vitaly@gnosis.io>
2022-11-25 07:50:52 +0100

    Use new collectibles v2 endpoint in token repository; rely on backend sorting

--- data/src/main/java/io/gnosis/data/repositories/TokenRepository.kt
+++ data/src/main/java/io/gnosis/data/repositories/TokenRepository.kt
@@ -1,6 +1,7 @@
 package io.gnosis.data.repositories
 
 import io.gnosis.data.backend.GatewayApi
+import io.gnosis.data.models.Page
 import io.gnosis.data.models.Safe
 import io.gnosis.data.models.assets.CoinBalances
 import io.gnosis.data.models.assets.Collectible
@@ -13,39 +14,10 @@ class TokenRepository(private val gatewayApi: GatewayApi) {
         return CoinBalances(response.fiatTotal, response.items)
     }
 
-    suspend fun loadCollectiblesOf(safe: Safe): List<Collectible> =
+    suspend fun getCollectibles(safe: Safe): Page<Collectible> =
         gatewayApi.loadCollectibles(chainId = safe.chainId, safeAddress = safe.address.asEthereumAddressChecksumString())
-            .asSequence()
-            .groupBy {
-                it.address
-            }
-            .toList()
-            .map {
-                it.second.sortedWith(Comparator { c1, c2 ->
-                    if (c1.name.isNullOrBlank()) {
-                        if (c2.name.isNullOrBlank())
-                            0
-                        else
-                            1
-                    } else if (c2.name.isNullOrBlank()) {
-                        -1
-                    } else {
-                        c1.name.compareTo(c2.name)
-                    }
-                })
-            }
-            .sortedWith(Comparator { l1, l2 ->
-                if (l1.first().tokenName.isBlank()) {
-                    if (l2.first().tokenName.isBlank())
-                        0
-                    else
-                        1
-                } else if (l2.first().tokenName.isBlank()) {
-                    -1
-                } else {
-                    l1.first().tokenName.compareTo(l2.first().tokenName)
-                }
-            })
-            .flatten()
-            .toList()
+
+
+    suspend fun loadCollectiblesPage(pageLink: String): Page<Collectible> =
+        gatewayApi.loadCollectiblesPage(pageLink)
 }
