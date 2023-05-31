f21a096edaa48a6c38a58db86d7fc652f418db64
Vitaly Katz <vitaly@gnosis.io>
2023-05-31 16:00:38 +0200

    Manage chain prefix settings in SettingsHandler

--- app/src/main/java/io/gnosis/safe/ui/settings/app/ChainPrefixAppSettingsFragment.kt
+++ app/src/main/java/io/gnosis/safe/ui/settings/app/ChainPrefixAppSettingsFragment.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import io.gnosis.safe.ScreenId
 import io.gnosis.safe.databinding.FragmentSettingsAppChainPrefixBinding
 import io.gnosis.safe.di.components.ViewComponent
 import io.gnosis.safe.ui.base.fragment.BaseViewBindingFragment
 import java.math.BigInteger
+import javax.inject.Inject
 
 class ChainPrefixAppSettingsFragment : BaseViewBindingFragment<FragmentSettingsAppChainPrefixBinding>() {
 
@@ -20,10 +22,26 @@ class ChainPrefixAppSettingsFragment : BaseViewBindingFragment<FragmentSettingsA
         component.inject(this)
     }
 
+    @Inject
+    lateinit var settingsHandler: SettingsHandler
+
     override fun inflateBinding(inflater: LayoutInflater, container: ViewGroup?): FragmentSettingsAppChainPrefixBinding =
         FragmentSettingsAppChainPrefixBinding.inflate(inflater, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        with(binding) {
+            backButton.setOnClickListener {
+                Navigation.findNavController(it).navigateUp()
+            }
+            prefixPrepend.settingSwitch.isChecked = settingsHandler.chainPrefixPrepend
+            prefixPrepend.settingSwitch.setOnCheckedChangeListener { _, isChecked ->
+                settingsHandler.chainPrefixPrepend = isChecked
+            }
+            prefixCopy.settingSwitch.isChecked = settingsHandler.chainPrefixCopy
+            prefixCopy.settingSwitch.setOnCheckedChangeListener { _, isChecked ->
+                settingsHandler.chainPrefixCopy = isChecked
+            }
+        }
     }
 }
