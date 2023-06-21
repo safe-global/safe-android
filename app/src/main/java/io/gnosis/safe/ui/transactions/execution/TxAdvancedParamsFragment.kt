ecdefec5a6649815e3eb5998063166951c0a0be8
Vitaly Katz <vitaly@gnosis.io>
2023-06-21 12:47:19 +0200

    Reuse advanced tx params fragment

--- app/src/main/java/io/gnosis/safe/ui/transactions/execution/TxAdvancedParamsFragment.kt
+++ app/src/main/java/io/gnosis/safe/ui/transactions/execution/TxAdvancedParamsFragment.kt
@@ -1,32 +1,8 @@
 package io.gnosis.safe.ui.transactions.execution
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import io.gnosis.safe.ScreenId
-import io.gnosis.safe.databinding.FragmentTxEditAdvancedParamsBinding
-import io.gnosis.safe.di.components.ViewComponent
-import io.gnosis.safe.ui.base.fragment.BaseViewBindingFragment
+import io.gnosis.safe.ui.transactions.details.AdvancedTransactionDetailsFragment
 
-class TxAdvancedParamsFragment : BaseViewBindingFragment<FragmentTxEditAdvancedParamsBinding>() {
-
+class TxAdvancedParamsFragment : AdvancedTransactionDetailsFragment() {
     override fun screenId() = ScreenId.TRANSACTIONS_EXEC_REVIEW_ADVANCED
-
-    override fun inject(component: ViewComponent) {
-        component.inject(this)
-    }
-
-    override fun inflateBinding(
-        inflater: LayoutInflater,
-        container: ViewGroup?
-    ): FragmentTxEditAdvancedParamsBinding =
-        FragmentTxEditAdvancedParamsBinding.inflate(inflater, container, false)
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        with(binding) {
-
-        }
-    }
 }
